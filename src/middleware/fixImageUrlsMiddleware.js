1e0e22013a2741b8cf912f4e20083b090f4e0483
Grzegorz Leoniec <greg@app-interactive.de>
2018-10-23 12:09:43 +0200

    improved schema and middleware

--- src/middleware/fixImageUrlsMiddleware.js
+++ src/middleware/fixImageUrlsMiddleware.js
@@ -1,14 +1,17 @@
 
 export const fixUrl = (url) => {
-  return url.replace('https://api-alpha.human-connection.org/uploads', 'http://localhost:3000/uploads')
+  return url.replace('https://api-alpha.human-connection.org', 'http://localhost:3000')
 }
-const fixImageURLs = (result, resolve, root, args, context, info) => {
-
-  if (result && typeof result === 'string' && result.indexOf('https://api-alpha.human-connection.org/uploads') === 0) {
+const fixImageURLs = (result, recursive) => {
+  if (result && typeof result === 'string' && result.indexOf('https://api-alpha.human-connection.org') === 0) {
     result = fixUrl(result)
+  } else if (result && Array.isArray(result)) {
+    result.forEach((res, index) => {
+      result[index] = fixImageURLs(result[index], true)
+    })
   } else if (result && typeof result === 'object') {
     Object.keys(result).forEach(key => {
-      result[key] = fixImageURLs(result[key])
+      result[key] = fixImageURLs(result[key], true)
     })
   }
   return result
@@ -17,11 +20,10 @@ const fixImageURLs = (result, resolve, root, args, context, info) => {
 export default {
   Mutation: async (resolve, root, args, context, info) => {
     const result = await resolve(root, args, context, info)
-    return fixImageURLs(result, resolve, root, args, context, info)
+    return fixImageURLs(result)
   },
   Query: async (resolve, root, args, context, info) => {
     let result = await resolve(root, args, context, info)
-
-    return fixImageURLs(result, resolve, root, args, context, info)
+    return fixImageURLs(result)
   }
 }
