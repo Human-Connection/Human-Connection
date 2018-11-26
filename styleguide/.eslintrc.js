efb95a16253287d1958c3d1dd8341035b147a77d
Grzegorz Leoniec <greg@app-interactive.de>
2018-11-26 15:51:27 +0100

    unified eslint settings

--- styleguide/.eslintrc.js
+++ styleguide/.eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true
-	},
-	extends: ['plugin:vue/strongly-recommended', '@vue/prettier'],
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-	},
-	parserOptions: {
-		parser: 'babel-eslint'
-	}
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: ['plugin:vue/recommended', 'plugin:prettier/recommended'],
+  // required to lint *.vue files
+  plugins: ['vue', 'prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 }
--- styleguide/babel.config.js
+++ styleguide/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['@vue/app'],
-  plugins: ["@babel/plugin-syntax-dynamic-import"]
+  plugins: ['@babel/plugin-syntax-dynamic-import']
 }
