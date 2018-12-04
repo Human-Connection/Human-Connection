ad62a8ebb324b32702c0776db7d2a7e36393586d
Robert Schäfer <git@roschaefer.de>
2018-12-05 00:12:47 +0100

    Set unimplemented mocha tests to pending

--- src/middleware/permissionsMiddleware.test.js
+++ src/middleware/permissionsMiddleware.test.js
@@ -1,19 +1,16 @@
 import { describe, it } from 'mocha'
-import { expect } from 'chai'
 
 describe('query', () => {
   describe('statistics', () => {
     describe('authenticated user', () => {
       describe('read', () => {
-        it('is forbidden', () => {
-        })
+        it('is forbidden')
       })
     })
 
-    context('admin', () => {
+    describe('admin', () => {
       describe('read', () => {
-        it('is permitted', () => {
-        })
+        it('is permitted')
       })
     })
   })
