bd26f738a59066deae87426cab12b44db8d82d02
Robert Schäfer <git@roschaefer.de>
2018-12-10 17:42:12 +0100

    Same as this commit:
    https://github.com/Human-Connection/Nitro-Web/pull/84/commits/8b324fc8dd0860c864838546b53cdd92a684b441

--- scripts/run_fullstack_tests.sh
+++ scripts/run_fullstack_tests.sh
@@ -3,7 +3,7 @@
 if [[ -v TRAVIS_TOKEN ]]; then
   # You need a Travis token to trigger the build on our meta repository.
   # This token will not be available on forks of this repo.
-  curl -L https://raw.githubusercontent.com/Human-Connection/Human-Connection/master/scripts/trigger_build.sh > trigger_build.sh
-  chmod +x trigger_build.sh
-  ./trigger_build.sh
+  curl -L https://raw.githubusercontent.com/Human-Connection/Human-Connection/master/scripts/trigger_build.sh > scripts/trigger_build.sh
+  chmod +x scripts/trigger_build.sh
+  ./scripts/trigger_build.sh
 fi
