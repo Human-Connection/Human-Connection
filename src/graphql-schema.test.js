6b216be05c560f1dff3554578ec880f93a02872b
Robert Schäfer <git@roschaefer.de>
2018-12-21 00:03:57 +0100

    Add @mattwr18's code and fix linting

--- src/graphql-schema.test.js
+++ src/graphql-schema.test.js
@@ -1,42 +1,42 @@
-import { request } from "graphql-request";
-import server from "./server";
+import { request } from 'graphql-request'
+import server from './server'
 
-describe("login", () => {
-  describe("asking for a `token`", () => {
-    describe("with valid email/password combination", () => {
-      xit("responds with a JWT token", () => {});
-    });
+describe('login', () => {
+  describe('asking for a `token`', () => {
+    describe('with valid email/password combination', () => {
+      xit('responds with a JWT token', () => {})
+    })
 
-    describe("with a valid email but incorrect password", () => {
+    describe('with a valid email but incorrect password', () => {
       beforeEach(() => {
         // create a user in the database
-      });
-      xit('responds with "Wrong email/password combination"', () => {});
-    });
+      })
+      xit('responds with "Wrong email/password combination"', () => {})
+    })
 
-    describe("with a non-existing email", () => {
-      let getHost;
+    describe('with a non-existing email', () => {
+      let getHost
 
       beforeEach(async () => {
-        const app = await server.start({ port: 0 });
-        const { port } = app.address();
-        getHost = () => `http://127.0.0.1:${port}`;
-      });
-
-      const email = "tom@bob.com";
-      const password = "jalksdf";
+        const app = await server.start({ port: 0 })
+        const { port } = app.address()
+        getHost = () => `http://127.0.0.1:${port}`
+      })
 
       const mutation = `
       mutation {
-        login(email:"asfasf@baldf.cm", password:"asdfasd"){
+        login(email:"user@example.com", password:"asdfasd"){
           token
         }
-      }`;
+      }`
 
       it('responds with "Wrong email/password combination"', async () => {
-        const response = await request(getHost(), mutation);
-        expect(response).toBe({});
-      });
-    });
-  });
-});
+        try {
+          await request(getHost(), mutation)
+        } catch (error) {
+          expect(error.response.errors[0].message).toEqual('Wrong email/password combination')
+        }
+      })
+    })
+  })
+})
