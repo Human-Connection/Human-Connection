18bfc85dcaa707ee9443f224f70d22cd3750eb3c
Grzegorz Leoniec <greg@app-interactive.de>
2018-12-28 12:55:42 +0100

    added 404 error, better error correction for oEmbed, audio and soundcloud

--- src/scraper.js
+++ src/scraper.js
@@ -11,11 +11,15 @@ const metascraper = require('metascraper')([
   require('metascraper-publisher')(),
   require('metascraper-title')(),
   require('metascraper-url')(),
+  require('metascraper-audio')(),
+  require('metascraper-soundcloud')(),
   require('metascraper-video')(),
   require('metascraper-youtube')()
 
   // require('./rules/metascraper-embed')()
 ])
+const { ApolloError } = require('apollo-server')
+
 const got = require('got')
 const request = require('request-promise-native')
 const find = require('lodash/find')
@@ -69,31 +73,46 @@ const scraper = {
       new Promise(async (resolve, reject) => {
         try {
           embed = await scraper.fetchEmbed(targetUrl)
+          console.log(embed)
           resolve()
         } catch(err) {
+          console.error(err)
           resolve()
         }
       })
     ])
-    return {
+
+    const output = {
       ...(removeEmptyAttrs(meta)),
       ...(removeEmptyAttrs(embed))
     }
+
+    if (isEmpty(output)) {
+      throw new ApolloError('Not found', 404)
+    }
+
+    return output
   },
   async fetchEmbed(targetUrl) {
     const url = urlParser.parse(targetUrl)
     const embedMeta = find(oEmbedProviders, provider => {
       return provider.provider_url.indexOf(url.hostname) >= 0
     })
+    console.log('embedMeta', embedMeta)
     if (!embedMeta) {
       return {}
     }
     const embedUrl = embedMeta.endpoints[0].url.replace('{format}', 'json')
 
-    let data = await request(`${embedUrl}?url=${targetUrl}`)
-    if (data) {
+    let data
+    try {
+      data = await request(`${embedUrl}?url=${targetUrl}`)
       data = JSON.parse(data)
-
+    } catch (err) {
+      data = await request(`${embedUrl}?url=${targetUrl}&format=json`)
+      data = JSON.parse(data)
+    }
+    if (data) {
       let output = {
         type: data.type,
         embed: data.html,
