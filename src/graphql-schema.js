77e77229b90f2babc6b9770e8bce63d7bc49de02
Grzegorz Leoniec <greg@app-interactive.de>
2019-01-07 17:30:54 +0100

    Fixed login and schema

--- src/graphql-schema.js
+++ src/graphql-schema.js
@@ -98,11 +98,12 @@ export const resolvers = {
       const session = driver.session()
       return session.run(
         'MATCH (user:User {email: $userEmail}) ' +
-        'RETURN user {.id, .slug, .name, .avatar, .locationId, .about, .email, .password, .role} as user LIMIT 1', { userEmail: email })
+        'RETURN user {.id, .slug, .name, .avatar, .locationName, .about, .email, .password, .role} as user LIMIT 1', {
+          userEmail: email
+        })
         .then(async (result) => {
           session.close()
           const [currentUser] = await result.records.map(function (record) {
-            console.log(record.get('user'))
             return record.get('user')
           })
           if (currentUser && await bcrypt.compareSync(password, currentUser.password)) {
