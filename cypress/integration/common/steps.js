aeed12a2bfde7571f30c6b032208f7e921235427
Grzegorz Leoniec <greg@app-interactive.de>
2019-01-08 18:47:14 +0100

    Fixed linting

--- cypress/integration/common/steps.js
+++ cypress/integration/common/steps.js
@@ -1,6 +1,5 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 import find from 'lodash/find'
-import { eq } from 'semver';
 
 /* global cy */
 
@@ -73,7 +72,6 @@ Given('my account has the following details:', table => {
 Given('my user account has the role {string}', role => {
   // TODO: use db factories instead of seed data
 })
-
 
 When('I log out', logout)
 
@@ -187,7 +185,8 @@ Then(
       .clear()
       .type(name)
     cy.contains('Save').click()
-})
+  }
+)
 When('I press {string}', label => {
   cy.contains(label).click()
 })
