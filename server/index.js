3382c5ef6f788d37fedcfb1e2c56f82820f747dc
Grzegorz Leoniec <greg@app-interactive.de>
2019-01-10 12:11:26 +0100

    Removed unused comment slashes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
-require('dotenv').config() //
+require('dotenv').config()
 
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
