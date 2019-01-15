1c04b8f3deb538195bec48f3a054d9e0e6563948
Robert Schäfer <git@roschaefer.de>
2019-01-16 00:47:28 +0100

    Import mongodb on-the-fly

--- db-migration-worker/import.sh
+++ db-migration-worker/import.sh
@@ -8,8 +8,6 @@ do
   fi
 done
 
-OUTPUT_FILE_NAME=${OUTPUT_FILE_NAME:-human-connection-dump}.archive
-
 echo "SSH_USERNAME             ${SSH_USERNAME}"
 echo "SSH_HOST                 ${SSH_HOST}"
 echo "MONGODB_USERNAME         ${MONGODB_USERNAME}"
@@ -21,11 +19,10 @@ echo "NEO4J_PASSWORD           ${NEO4J_PASSWORD}"
 echo "-------------------------------------------------"
 
 ssh -4 -M -S my-ctrl-socket -fnNT -L 27018:localhost:27017 -l ${SSH_USERNAME} ${SSH_HOST}
-mongodump --host localhost -d ${MONGODB_DATABASE} --port 27018 --username ${MONGODB_USERNAME} --password ${MONGODB_PASSWORD} --authenticationDatabase ${MONGODB_AUTH_DB} --gzip --archive=${OUTPUT_FILE_NAME}
+mongodump --host localhost -d ${MONGODB_DATABASE} --port 27018 --username ${MONGODB_USERNAME} --password ${MONGODB_PASSWORD} --authenticationDatabase ${MONGODB_AUTH_DB} --gzip --archive | mongorestore --gzip --archive
 ssh -S my-ctrl-socket -O check -l ${SSH_USERNAME} ${SSH_HOST}
 ssh -S my-ctrl-socket -O exit  -l ${SSH_USERNAME} ${SSH_HOST}
 
-mongorestore --gzip --archive=human-connection-dump.archive
 # cat ./neo4j_import.cql | /usr/share/neo4j/bin/cypher-shell
 
 
