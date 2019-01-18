e6326f772b67059e49b781384954a2e6f6dce744
Grzegorz Leoniec <greg@app-interactive.de>
2019-01-18 10:35:48 +0100

    Secured moderation and admin pages by role

--- pages/admin.vue
+++ pages/admin.vue
@@ -21,6 +21,7 @@
 
 <script>
 export default {
+  middleware: ['isAdmin'],
   computed: {
     routes() {
       return [
--- pages/moderation.vue
+++ pages/moderation.vue
@@ -21,6 +21,7 @@
 
 <script>
 export default {
+  middleware: ['isModerator'],
   computed: {
     routes() {
       return [
