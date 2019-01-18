baac0748a002d64b2df56b7d6cf3eecdd936552a
Grzegorz Leoniec <greg@app-interactive.de>
2019-01-18 15:13:13 +0100

    Added comming soon empty state to setting, admin and take action pages

--- pages/post/_slug/take-action.vue
+++ pages/post/_slug/take-action.vue
@@ -1,52 +1,18 @@
 <template>
   <ds-card header="Werde aktiv!">
-    <p>Was kann ich tun? Hier findest du mögliche Projekte, Aktioner, etc.</p>
+    <hc-empty
+      icon="tasks"
+      message="Comming Soon…"
+    />
   </ds-card>
 </template>
 
 <script>
-import gql from 'graphql-tag'
+import HcEmpty from '~/components/Empty.vue'
 
 export default {
-  transition: {
-    name: 'slide-up',
-    mode: 'out-in'
-  },
-
-  data() {
-    return {
-      Post: []
-    }
-  },
-  computed: {
-    tags() {
-      return this.Post ? this.Post[0].tags.map(tag => tag.name) : '-'
-    }
-  },
-  apollo: {
-    Post: {
-      query: gql(`
-        query {
-          Post {
-            id
-            title
-            tags {
-              name
-            }
-            categories {
-              name
-            }
-            shoutedCount
-            shoutedBy {
-              name
-              friends {
-                name
-              }
-            }
-          }
-        }
-      `)
-    }
+  components: {
+    HcEmpty
   }
 }
 </script>
--- pages/settings/languages.vue
+++ pages/settings/languages.vue
@@ -1,7 +1,18 @@
 <template>
-  <ds-card>
-    <ds-space margin="small">
-      {{ $t('settings.languages.name') }}
-    </ds-space>
+  <ds-card :header="$t('settings.languages.name')">
+    <hc-empty
+      icon="tasks"
+      message="Comming Soon…"
+    />
   </ds-card>
 </template>
+
+<script>
+import HcEmpty from '~/components/Empty.vue'
+
+export default {
+  components: {
+    HcEmpty
+  }
+}
+</script>
