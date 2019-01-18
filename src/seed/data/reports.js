4c265bf3b8395735087406aac16e302ea83b6721
Grzegorz Leoniec <greg@app-interactive.de>
2019-01-18 11:22:44 +0100

    Fixed report seeding

--- src/seed/data/reports.js
+++ src/seed/data/reports.js
@@ -1,26 +1,26 @@
 export default function (data) {
   return `
     mutation {
-      r1: CreateReport(id: "r1", description: "Bad Stuff") {
+      r1: CreateReport(id: "r1", type: contribution, description: "Bad Stuff") {
         id
       }
-      r2: CreateReport(id: "r2", description: "Please remove this sh**") {
+      r2: CreateReport(id: "r2", type: comment, description: "Please remove this sh**") {
         id
       }
-      r3: CreateReport(id: "r3", description: "The user have misbehaved!") {
+      r3: CreateReport(id: "r3", type: user, description: "The user have misbehaved!") {
         id
       }
-      ra1: AddReportAuthor(from: { id: "u1" }, to: { id: "r1" }) {
+      ra1: AddReportReporter(from: { id: "u1" }, to: { id: "r1" }) {
         from {
           id
         }
       }
-      ra2: AddReportAuthor(from: { id: "u2" }, to: { id: "r2" }) {
+      ra2: AddReportReporter(from: { id: "u2" }, to: { id: "r2" }) {
         from {
           id
         }
       }
-      ra3: AddReportAuthor(from: { id: "u3" }, to: { id: "r3" }) {
+      ra3: AddReportReporter(from: { id: "u3" }, to: { id: "r3" }) {
         from {
           id
         }
