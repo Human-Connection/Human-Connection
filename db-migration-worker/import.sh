f0c18e2ccabae1438ba9ba88ce9c1cbe85e09d19
Robert Schäfer <git@roschaefer.de>
2019-01-18 21:07:24 +0100

    Clean up Dockerfile, add known_hosts file
    @appinteractive it's troublesome to add the SSH private key via
    environment variable. You have to convert newlines to spaces and convert
    them back - which I think is error prone. I hope we can transfer the
    private key file on to our deployed container later on.

--- db-migration-worker/import.sh
+++ db-migration-worker/import.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-for var in "SSH_USERNAME" "SSH_HOST" "MONGODB_USERNAME" "MONGODB_PASSWORD" "MONGODB_DATABASE" "NEO4J_USERNAME" "NEO4J_PASSWORD" "MONGODB_AUTH_DB"
+for var in "SSH_USERNAME" "SSH_HOST" "MONGODB_USERNAME" "MONGODB_PASSWORD" "MONGODB_DATABASE" "MONGODB_AUTH_DB"
 do
   if [[ -z "${!var}" ]]; then
     echo "${var} is undefined"
@@ -14,9 +14,10 @@ echo "MONGODB_USERNAME         ${MONGODB_USERNAME}"
 echo "MONGODB_PASSWORD         ${MONGODB_PASSWORD}"
 echo "MONGODB_DATABASE         ${MONGODB_DATABASE}"
 echo "MONGODB_AUTH_DB          ${MONGODB_AUTH_DB}"
-echo "NEO4J_USERNAME           ${NEO4J_USERNAME}"
-echo "NEO4J_PASSWORD           ${NEO4J_PASSWORD}"
 echo "-------------------------------------------------"
+
+mongo ${MONGODB_DATABASE} --eval "db.dropDatabase();"
+rm -f /mongo-export/*
 
 ssh -4 -M -S my-ctrl-socket -fnNT -L 27018:localhost:27017 -l ${SSH_USERNAME} ${SSH_HOST}
 mongodump --host localhost -d ${MONGODB_DATABASE} --port 27018 --username ${MONGODB_USERNAME} --password ${MONGODB_PASSWORD} --authenticationDatabase ${MONGODB_AUTH_DB} --gzip --archive | mongorestore --gzip --archive
