3aef664c8c087e4ea6a734189cad21ec730a3468
Robert Schäfer <git@roschaefer.de>
2019-01-20 23:18:42 +0100

    Import collections in order

--- neo4j/import/import.sh
+++ neo4j/import/import.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r;" | cypher-shell
-cat $SCRIPT_DIRECTORY/*.cql                            | cypher-shell
+for collection in "users" "contributions" "comments"
+do
+  echo "Import ${collection}..." && cat $SCRIPT_DIRECTORY/$collection.cql | cypher-shell
+done
