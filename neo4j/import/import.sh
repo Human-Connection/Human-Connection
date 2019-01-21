494748c4c459f886e78a491c6f022e2a0da96ac0
Robert Schäfer <git@roschaefer.de>
2019-01-21 23:43:23 +0100

    Import shouts

--- neo4j/import/import.sh
+++ neo4j/import/import.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r;" | cypher-shell
-for collection in "badges" "categories" "users" "follows" "contributions" "comments"
+for collection in "badges" "categories" "users" "follows" "contributions" "shouts" "comments"
 do
   echo "Import ${collection}..." && cypher-shell < $SCRIPT_DIRECTORY/$collection.cql
 done
