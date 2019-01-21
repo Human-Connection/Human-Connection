2cab6fe9cf13fa8582d33eda8c1a7c73b4992c5f
Robert Schäfer <git@roschaefer.de>
2019-01-21 22:33:32 +0100

    Replace `scp` with `rsync`
    `rsync` is faster on multiple runs. The `--update` flag will only
    download newer images and does not overwrite existing files. Thus, it will
    only download missing images.

--- db-migration-worker/import.sh
+++ db-migration-worker/import.sh
@@ -22,9 +22,10 @@ rm -f /mongo-export/*
 
 ssh -4 -M -S my-ctrl-socket -fnNT -L 27018:localhost:27017 -l ${SSH_USERNAME} ${SSH_HOST}
 mongodump --host localhost -d ${MONGODB_DATABASE} --port 27018 --username ${MONGODB_USERNAME} --password ${MONGODB_PASSWORD} --authenticationDatabase ${MONGODB_AUTH_DB} --gzip --archive | mongorestore --gzip --archive
-scp -r ${SSH_USERNAME}@${SSH_HOST}:${UPLOADS_DIRECTORY}/* /uploads/
 ssh -S my-ctrl-socket -O check -l ${SSH_USERNAME} ${SSH_HOST}
 ssh -S my-ctrl-socket -O exit  -l ${SSH_USERNAME} ${SSH_HOST}
+
+rsync --archive --update --verbose ${SSH_USERNAME}@${SSH_HOST}:${UPLOADS_DIRECTORY}/* /uploads/
 
 for collection in "categories" "badges" "users" "contributions" "comments" "follows" "shouts"
 do
