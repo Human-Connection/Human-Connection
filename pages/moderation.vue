4af7a6673cbfc1719643357d2be5efd36cdd9749
Grzegorz Leoniec <greg@app-interactive.de>
2019-01-21 12:06:38 +0100

    Mobile friendly admin, settings and moderation

--- pages/moderation.vue
+++ pages/moderation.vue
@@ -4,10 +4,10 @@
       {{ $t('moderation.name') }}
     </ds-heading>
     <ds-flex gutter="small">
-      <ds-flex-item :width="{ base: '200px' }">
+      <ds-flex-item :width="{ base: '100%', md: '200px' }">
         <ds-menu :routes="routes" />
       </ds-flex-item>
-      <ds-flex-item>
+      <ds-flex-item :width="{ base: '100%', md: 1 }">
         <transition
           name="slide-up"
           appear
