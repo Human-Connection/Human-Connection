509e4feeb9ec6c289a7fd49d097aa5e8182ea94a
Robert Schäfer <git@roschaefer.de>
2019-01-21 22:12:50 +0100

    Add workflow to download remote uploads
    * remote uploads directory
    * download content of uploads directory with `scp`
    * share uploads volume with backend container

--- db-migration-worker/import.sh
+++ db-migration-worker/import.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for var in "SSH_USERNAME" "SSH_HOST" "MONGODB_USERNAME" "MONGODB_PASSWORD" "MONGODB_DATABASE" "MONGODB_AUTH_DB"
+for var in "SSH_USERNAME" "SSH_HOST" "MONGODB_USERNAME" "MONGODB_PASSWORD" "MONGODB_DATABASE" "MONGODB_AUTH_DB" "UPLOADS_DIRECTORY"
 do
   if [[ -z "${!var}" ]]; then
     echo "${var} is undefined"
@@ -14,6 +14,7 @@ echo "MONGODB_USERNAME         ${MONGODB_USERNAME}"
 echo "MONGODB_PASSWORD         ${MONGODB_PASSWORD}"
 echo "MONGODB_DATABASE         ${MONGODB_DATABASE}"
 echo "MONGODB_AUTH_DB          ${MONGODB_AUTH_DB}"
+echo "UPLOADS_DIRECTORY        ${UPLOADS_DIRECTORY}"
 echo "-------------------------------------------------"
 
 mongo ${MONGODB_DATABASE} --eval "db.dropDatabase();"
@@ -21,6 +22,7 @@ rm -f /mongo-export/*
 
 ssh -4 -M -S my-ctrl-socket -fnNT -L 27018:localhost:27017 -l ${SSH_USERNAME} ${SSH_HOST}
 mongodump --host localhost -d ${MONGODB_DATABASE} --port 27018 --username ${MONGODB_USERNAME} --password ${MONGODB_PASSWORD} --authenticationDatabase ${MONGODB_AUTH_DB} --gzip --archive | mongorestore --gzip --archive
+scp -r ${SSH_USERNAME}@${SSH_HOST}:${UPLOADS_DIRECTORY}/* /uploads/
 ssh -S my-ctrl-socket -O check -l ${SSH_USERNAME} ${SSH_HOST}
 ssh -S my-ctrl-socket -O exit  -l ${SSH_USERNAME} ${SSH_HOST}
 
