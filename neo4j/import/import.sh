ce7ce4027265189dd2ed32f30bf1da44ea6c722d
Robert Schäfer <git@roschaefer.de>
2019-01-21 23:26:45 +0100

    Import badges

--- neo4j/import/import.sh
+++ neo4j/import/import.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r;" | cypher-shell
-for collection in "categories" "users" "contributions" "comments"
+for collection in "badges" "categories" "users" "contributions" "comments"
 do
   echo "Import ${collection}..." && cypher-shell < $SCRIPT_DIRECTORY/$collection.cql
 done
