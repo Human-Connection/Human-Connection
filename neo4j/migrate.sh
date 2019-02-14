d579d19ad24764f0de3f6ab87da809a0b2e8bbef
Robert Schäfer <git@roschaefer.de>
2019-02-14 17:30:17 +0100

    Modify migrate for development environment

--- neo4j/migrate.sh
+++ neo4j/migrate.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
-echo "CALL dbms.security.changePassword('${NEO4J_PASSWORD}');" | cypher-shell --username neo4j --password neo4j
-echo 'CALL db.index.fulltext.createNodeIndex("full_text_search",["Post"],["title", "content"]);' | cypher-shell --username neo4j --password $NEO4J_PASSWORD
+if [[ -z "${NEO4J_PASSWORD}" ]]; then
+  echo 'CALL db.index.fulltext.createNodeIndex("full_text_search",["Post"],["title", "content"]);' | cypher-shell
+else
+  echo "CALL dbms.security.changePassword('${NEO4J_PASSWORD}');" | cypher-shell --username neo4j --password neo4j
+  echo 'CALL db.index.fulltext.createNodeIndex("full_text_search",["Post"],["title", "content"]);' | cypher-shell --username neo4j --password $NEO4J_PASSWORD
+fi
