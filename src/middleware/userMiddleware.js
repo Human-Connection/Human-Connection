f1dd52f5791c2889cda0ac1f2eace85c4543edc4
Robert Schäfer <git@roschaefer.de>
2019-02-20 00:38:48 +0100

    Expose yet another bug in the authorization
    Cannot read property id of null
    
    CC @appinteractive

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -11,21 +11,6 @@ export default {
       const result = await resolve(root, args, context, info)
       await createOrUpdateLocations(args.id, args.locationName, context.driver)
       return result
-    },
-    CreatePost: async (resolve, root, args, context, info) => {
-      const result = await resolve(root, args, context, info)
-
-      const session = context.driver.session()
-      await session.run(
-        'MATCH (author:User {id: $userId}), (post:Post {id: $postId}) ' +
-        'MERGE (post)<-[:WROTE]-(author) ' +
-        'RETURN author', {
-          userId: context.user.id,
-          postId: result.id
-        })
-      session.close()
-
-      return result
     }
   }
 }
