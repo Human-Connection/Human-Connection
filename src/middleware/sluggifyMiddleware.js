3532b473eeaa2dcfa12e50de5a25cd00c4fda75a
Robert Schäfer <git@roschaefer.de>
2019-02-20 01:30:32 +0100

    Choose another slug is green

--- src/middleware/sluggifyMiddleware.js
+++ src/middleware/sluggifyMiddleware.js
@@ -1,11 +1,22 @@
+import uniqueSlug from './slugify/uniqueSlug'
 import slug from 'slug'
+
+const isUniqueFor = (context, type) => {
+  return async (slug) => {
+    const session = context.driver.session()
+    const response = await session.run(
+      `MATCH(p:${type} {slug: $slug }) return p.slug`, {
+        slug
+      })
+    session.close()
+    return response.records.length === 0
+  }
+}
 
 export default {
   Mutation: {
     CreatePost: async (resolve, root, args, context, info) => {
-      args.slug = slug(args.title, {
-        lower: true
-      })
+      args.slug = await uniqueSlug(args.title, isUniqueFor(context, 'Post'))
       const result = await resolve(root, args, context, info)
       return result
     },
