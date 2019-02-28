8febf147cef2dd3eccb3b3331f47add0c02d6988
Robert Schäfer <git@roschaefer.de>
2019-02-28 02:53:11 +0100

    Fix lint

--- src/middleware/softDeleteMiddleware.spec.js
+++ src/middleware/softDeleteMiddleware.spec.js
@@ -9,13 +9,13 @@ let action
 
 beforeEach(async () => {
   await Promise.all([
-    factory.create('User', { role: 'user',      email: 'user@example.org',      password: '1234' }),
+    factory.create('User', { role: 'user', email: 'user@example.org', password: '1234' }),
     factory.create('User', { role: 'moderator', email: 'moderator@example.org', password: '1234' })
   ])
-  await factory.authenticateAs({email: 'user@example.org', password: '1234'})
+  await factory.authenticateAs({ email: 'user@example.org', password: '1234' })
   await Promise.all([
-    factory.create('Post', { title: 'Deleted post',          deleted: true,  disabled: false }),
-    factory.create('Post', { title: 'Disabled post',         deleted: false, disabled: true}),
+    factory.create('Post', { title: 'Deleted post', deleted: true, disabled: false }),
+    factory.create('Post', { title: 'Disabled post', deleted: false, disabled: true }),
     factory.create('Post', { title: 'Publicly visible post', deleted: false, disabled: false })
   ])
 })
@@ -41,7 +41,7 @@ describe('softDeleteMiddleware', () => {
       })
 
       it('hides deleted or disabled posts', async () => {
-        const expected = {Post: [{title: 'Publicly visible post'}]}
+        const expected = { Post: [{ title: 'Publicly visible post' }] }
         await expect(action()).resolves.toEqual(expected)
       })
     })
@@ -53,7 +53,7 @@ describe('softDeleteMiddleware', () => {
       })
 
       it('hides deleted or disabled posts', async () => {
-        const expected = {Post: [{title: 'Publicly visible post'}]}
+        const expected = { Post: [{ title: 'Publicly visible post' }] }
         await expect(action()).resolves.toEqual(expected)
       })
     })
@@ -81,7 +81,7 @@ describe('softDeleteMiddleware', () => {
         })
 
         it('shows deleted posts', async () => {
-          const expected = {Post: [{title: 'Deleted post'}]}
+          const expected = { Post: [{ title: 'Deleted post' }] }
           await expect(action()).resolves.toEqual(expected)
         })
       })
@@ -110,7 +110,7 @@ describe('softDeleteMiddleware', () => {
         })
 
         it('shows disabled posts', async () => {
-          const expected = {Post: [{title: 'Disabled post'}]}
+          const expected = { Post: [{ title: 'Disabled post' }] }
           await expect(action()).resolves.toEqual(expected)
         })
       })
