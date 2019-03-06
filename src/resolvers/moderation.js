f40a67b7a85c6aa24ea7d005e62cf6ce29fb82ab
Robert Schäfer <git@roschaefer.de>
2019-03-06 17:12:30 +0100

    Implement disabling of comments+users+posts

--- src/resolvers/moderation.js
+++ src/resolvers/moderation.js
@@ -1,28 +1,28 @@
 export default {
   Mutation: {
     disable: async (object, params, { user, driver }) => {
-      const { resource: { id: postId } } = params
+      const { resource: { id } } = params
       const { id: userId } = user
       const cypher = `
       MATCH (u:User {id: $userId})
-      MATCH (p:Post {id: $postId})
-      SET p.disabled = true
-      MERGE (p)<-[:DISABLED]-(u)
+      MATCH (r {id: $id})
+      SET r.disabled = true
+      MERGE (r)<-[:DISABLED]-(u)
       `
       const session = driver.session()
-      const res = await session.run(cypher, { postId, userId })
+      const res = await session.run(cypher, { id, userId })
       session.close()
       return Boolean(res)
     },
     enable: async (object, params, { user, driver }) => {
-      const { resource: { id: postId } } = params
+      const { resource: { id } } = params
       const cypher = `
-      MATCH (p:Post {id: $postId})<-[d:DISABLED]-()
+      MATCH (p {id: $id})<-[d:DISABLED]-()
       SET p.disabled = false
       DELETE d
       `
       const session = driver.session()
-      const res = await session.run(cypher, { postId })
+      const res = await session.run(cypher, { id })
       session.close()
       return Boolean(res)
     }
