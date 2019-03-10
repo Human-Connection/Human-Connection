4cc02a620033052af4a8c0dc53940ae56bcc02b7
Robert Schäfer <git@roschaefer.de>
2019-03-10 15:18:47 +0100

    Import ReportModal in Modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -5,17 +5,24 @@
       :resource="data"
       @close="close"
     />
+    <report-modal
+      :is-open="open === 'report'"
+      :resource="data"
+      @close="close"
+    />
   </div>
 </template>
 
 <script>
 import DisableModal from '~/components/Modal/DisableModal'
+import ReportModal from '~/components/Modal/ReportModal'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'Modal',
   components: {
-    DisableModal
+    DisableModal,
+    ReportModal
   },
   computed: {
     ...mapGetters({
