ab09f356caf33d87dc9297d1457642b667b6fe19
Grzegorz Leoniec <greg@app-interactive.de>
2019-03-12 17:59:40 +0100

    Fixed a submit buttion translation for reporting content

--- cypress/integration/common/report.js
+++ cypress/integration/common/report.js
@@ -112,7 +112,7 @@ When(/^I confirm the reporting dialog .*:$/, message => {
   cy.contains(message) // wait for element to become visible
   cy.get('.ds-modal').within(() => {
     cy.get('button')
-      .contains('Send Report')
+      .contains('Report')
       .click()
   })
 })
