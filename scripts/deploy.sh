a8c3fae17b953542e7a8bbfbcb71f1aecd060e05
Robert Schäfer <git@roschaefer.de>
2019-03-22 09:24:48 +0100

    Specify path to `patch-deployment.yaml`

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sed -i "s/<COMMIT>/${TRAVIS_COMMIT}/g" patch-deployment.yaml
-kubectl --namespace=human-connection patch deployment nitro-backend  -p "$(cat patch-deployment.yaml)"
-kubectl --namespace=human-connection patch deployment nitro-web  -p "$(cat patch-deployment.yaml)"
+kubectl --namespace=human-connection patch deployment nitro-backend  -p "$(cat $TRAVIS_BUILD_DIR/scripts/patch-deployment.yaml)"
+kubectl --namespace=human-connection patch deployment nitro-web  -p "$(cat $TRAVIS_BUILD_DIR/scripts/patch-deployment.yaml)"
