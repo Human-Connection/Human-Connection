ebb1c283b7e68167ed0380830caaf7df0ff9b9ed
Robert Schäfer <git@roschaefer.de>
2019-03-25 15:12:27 +0100

    Refactor: Use URL.host instead of hostname
    This way, we don't have to manually add the port number.
    
    cc @Mastercuber
    
    Documentation: https://nodejs.org/api/url.html#url_urlobject_host

--- backend/src/activitypub/ActivityPub.js
+++ backend/src/activitypub/ActivityPub.js
@@ -22,9 +22,8 @@ let activityPub = null
 export { activityPub }
 
 export default class ActivityPub {
-  constructor (hostname, port, uri) {
-    this.hostname = hostname
-    this.port = port
+  constructor (host, uri) {
+    this.host = host
     this.dataSource = new NitroDataSource(uri)
     this.collections = new Collections(this.dataSource)
   }
@@ -33,7 +32,7 @@ export default class ActivityPub {
     if (!activityPub) {
       dotenv.config()
       const url = new URL(process.env.GRAPHQL_URI)
-      activityPub = new ActivityPub(url.host || 'localhost', url.port || 4000, url.origin)
+      activityPub = new ActivityPub(url.host || 'localhost:4000', url.origin)
 
       // integrate into running graphql express server
       server.express.set('ap', activityPub)
@@ -59,7 +58,7 @@ export default class ActivityPub {
         }
       }, async (err, response, toActorObject) => {
         if (err) return reject(err)
-        debug(`name = ${toActorName}@${this.hostname}`)
+        debug(`name = ${toActorName}@${this.host}`)
         // save shared inbox
         toActorObject = JSON.parse(toActorObject)
         await this.dataSource.addSharedInboxEndpoint(toActorObject.endpoints.sharedInbox)
@@ -184,7 +183,7 @@ export default class ActivityPub {
   }
 
   generateStatusId (slug) {
-    return `https://${this.hostname}/activitypub/users/${slug}/status/${uuid()}`
+    return `https://${this.host}/activitypub/users/${slug}/status/${uuid()}`
   }
 
   async sendActivity (activity) {
--- backend/src/activitypub/utils/actor.js
+++ backend/src/activitypub/utils/actor.js
@@ -6,21 +6,21 @@ export function createActor (name, pubkey) {
       'https://www.w3.org/ns/activitystreams',
       'https://w3id.org/security/v1'
     ],
-    'id': `https://${activityPub.hostname}/activitypub/users/${name}`,
+    'id': `https://${activityPub.host}/activitypub/users/${name}`,
     'type': 'Person',
     'preferredUsername': `${name}`,
     'name': `${name}`,
-    'following': `https://${activityPub.hostname}/activitypub/users/${name}/following`,
-    'followers': `https://${activityPub.hostname}/activitypub/users/${name}/followers`,
-    'inbox': `https://${activityPub.hostname}/activitypub/users/${name}/inbox`,
-    'outbox': `https://${activityPub.hostname}/activitypub/users/${name}/outbox`,
-    'url': `https://${activityPub.hostname}/activitypub/@${name}`,
+    'following': `https://${activityPub.host}/activitypub/users/${name}/following`,
+    'followers': `https://${activityPub.host}/activitypub/users/${name}/followers`,
+    'inbox': `https://${activityPub.host}/activitypub/users/${name}/inbox`,
+    'outbox': `https://${activityPub.host}/activitypub/users/${name}/outbox`,
+    'url': `https://${activityPub.host}/activitypub/@${name}`,
     'endpoints': {
-      'sharedInbox': `https://${activityPub.hostname}/activitypub/inbox`
+      'sharedInbox': `https://${activityPub.host}/activitypub/inbox`
     },
     'publicKey': {
-      'id': `https://${activityPub.hostname}/activitypub/users/${name}#main-key`,
-      'owner': `https://${activityPub.hostname}/activitypub/users/${name}`,
+      'id': `https://${activityPub.host}/activitypub/users/${name}#main-key`,
+      'owner': `https://${activityPub.host}/activitypub/users/${name}`,
       'publicKeyPem': pubkey
     }
   }
@@ -28,12 +28,12 @@ export function createActor (name, pubkey) {
 
 export function createWebFinger (name) {
   return {
-    'subject': `acct:${name}@${activityPub.hostname}`,
+    'subject': `acct:${name}@${activityPub.host}`,
     'links': [
       {
         'rel': 'self',
         'type': 'application/activity+json',
-        'href': `https://${activityPub.hostname}/activitypub/users/${name}`
+        'href': `https://${activityPub.host}/activitypub/users/${name}`
       }
     ]
   }
