2dc5252f38a952bf3d5282789446ee7f3e9a788b
Robert Schäfer <git@roschaefer.de>
2019-04-03 18:09:39 +0200

    Install everything on Travis CI through snap
    This commit should setup kubectl in a way that it downloads a recent
    `kubeconfig` from Digitial Ocean with `doctl`. Thus, deployments will
    not fail after a kubeconfig has expired.
    
    Since it seems to be the recommended way to install `doctl` through
    [Snap](https://snapcraft.io/). I decided to install everything else
    through snap too, including chrome and docker.

--- scripts/install_kubectl.sh
+++ scripts/install_kubectl.sh
@@ -4,11 +4,6 @@
 # so let's exit successfully if kubectl is already installed:
 command -v kubectl && exit 0
 
-
-openssl aes-256-cbc -K $encrypted_44f54ef0bc46_key -iv $encrypted_44f54ef0bc46_iv -in kubeconfig.yaml.enc -out kubeconfig.yaml -d
-mkdir ${HOME}/.kube
-cp kubeconfig.yaml ${HOME}/.kube/config
-
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
