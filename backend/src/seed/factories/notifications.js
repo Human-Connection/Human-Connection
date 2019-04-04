8477e71c28019c1226c9e788a051ec2994ee4006
Robert Schäfer <git@roschaefer.de>
2019-04-05 01:02:00 +0200

    Sketch test for #350

---
+++ backend/src/seed/factories/notifications.js
@@ -0,0 +1,18 @@
+import faker from 'faker'
+import uuid from 'uuid/v4'
+
+export default function (params) {
+  const {
+    id = uuid(),
+    read = false
+  } = params
+
+  return `
+    mutation {
+      CreateNotification(
+        id: "${id}",
+        read: ${read},
+      ) { id, read }
+    }
+  `
+}
