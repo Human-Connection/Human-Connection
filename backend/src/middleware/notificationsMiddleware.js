f372cdcbdba94820407f69d1cfa727d2093e5b47
Robert Schäfer <git@roschaefer.de>
2019-04-08 12:01:09 +0200

    Avoid to send out notifications for email adresses

--- backend/src/middleware/notificationsMiddleware.js
+++ backend/src/middleware/notificationsMiddleware.js
@@ -1,16 +1,12 @@
-const MENTION_REGEX = /@(\S+)/g
+import { extractSlugs } from './notifications/mentions'
 
 const notify = async (resolve, root, args, context, resolveInfo) => {
   const post = await resolve(root, args, context, resolveInfo)
 
   const session = context.driver.session()
   const { content, id: postId } = post
-  const slugs = []
+  const slugs = extractSlugs(content)
   const createdAt = (new Date()).toISOString()
-  let match
-  while ((match = MENTION_REGEX.exec(content)) != null) {
-    slugs.push(match[1])
-  }
   const cypher = `
     match(u:User) where u.slug in $slugs
     match(p:Post) where p.id = $postId
