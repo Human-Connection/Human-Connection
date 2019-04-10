587025e4d33326dd0ee93815f6f132487686b6c5
Wolfgang Huß <wolle.huss@pjannto.com>
2019-04-10 11:15:45 +0200

    Start refactoring follow

--- backend/src/resolvers/follow.js
+++ backend/src/resolvers/follow.js
@@ -1,49 +1,49 @@
+import gql from 'graphql-tag'
+import { neo4jgraphql } from 'neo4j-graphql-js'
+
 export default {
-    Mutation: {
-      follow: async (_object, params, context, _resolveInfo) => {
-        const { followedId, followedType } = params
-        const session = context.driver.session()
-  
-        let sessionRes = await session.run(
-          `MATCH (n {id: $id}), (u:User {id: $cypherParams.currentUserId})
-           WHERE $type IN labels(n) AND NOT $id = $cypherParams.currentUserId
-           MERGE (u)-[r:FOLLOWS]->(n)
-           RETURN COUNT(r) > 0`,
-          {
-            badgeId: fromBadgeId,
-            rewardedUserId: toUserId
-          }
-        )
-  
-        const [rewardedUser] = sessionRes.records.map(record => {
-          return record.get('rewardedUser')
-        })
-  
-        session.close()
-  
-        return rewardedUser.id
-      },
-  
-      unfollow: async (_object, params, context, _resolveInfo) => {
-        const { fromBadgeId, toUserId } = params
-        const session = context.driver.session()
-  
-        let sessionRes = await session.run(
-          `MATCH (badge:Badge {id: $badgeId})-[reward:REWARDED]->(rewardedUser:User {id: $rewardedUserId})
-          DELETE reward
-          RETURN rewardedUser {.id}`,
-          {
-            badgeId: fromBadgeId,
-            rewardedUserId: toUserId
-          }
-        )
-        const [rewardedUser] = sessionRes.records.map(record => {
-          return record.get('rewardedUser')
-        })
-        session.close()
-  
-        return rewardedUser.id
-      }
+  Mutation: {
+    follow: async (object, params, context, resolveInfo) => {
+      const result = await neo4jgraphql(object, params, context, resolveInfo, true)
+      const { followedId, followedType } = params
+
+      const session = context.driver.session()
+      await session.run(
+        gql`
+          MATCH (n {id: $followedId}), (u:User {id: $userId})
+          WHERE $type IN labels(n) AND NOT $id = $userId
+          MERGE (u)-[r:FOLLOWS]->(n)
+          RETURN COUNT(r) > 0`,
+        {
+          followedId: followedId,
+          type: followedType,
+          userId: context.user.id
+        }
+      )
+      session.close()
+
+      return result
     }
+
+    // unfollow: async (_object, params, context, _resolveInfo) => {
+    //   const { fromBadgeId, toUserId } = params
+    //   const session = context.driver.session()
+
+    //   let sessionRes = await session.run(
+    //     `MATCH (badge:Badge {id: $badgeId})-[reward:REWARDED]->(rewardedUser:User {id: $rewardedUserId})
+    //       DELETE reward
+    //       RETURN rewardedUser {.id}`,
+    //     {
+    //       badgeId: fromBadgeId,
+    //       rewardedUserId: toUserId
+    //     }
+    //   )
+    //   const [rewardedUser] = sessionRes.records.map(record => {
+    //     return record.get('rewardedUser')
+    //   })
+    //   session.close()
+
+    //   return rewardedUser.id
+    // }
   }
-  
+}
