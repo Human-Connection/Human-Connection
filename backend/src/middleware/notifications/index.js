f86e98b32e671a57b949c3fe66ac7c1fc1f075f3
Robert Schäfer <git@roschaefer.de>
2019-04-16 17:48:42 +0200

    Implement right order of notifications middleware

--- backend/src/middleware/notifications/index.js
+++ backend/src/middleware/notifications/index.js
@@ -1,20 +1,20 @@
 import extractIds from './extractMentions'
 
 const notify = async (resolve, root, args, context, resolveInfo) => {
+  const ids = extractIds(args.content) // before mention class gets removed
   const post = await resolve(root, args, context, resolveInfo)
 
   const session = context.driver.session()
   const { content, id: postId } = post
-  const slugs = extractIds(content)
   const createdAt = (new Date()).toISOString()
   const cypher = `
-    match(u:User) where u.slug in $slugs
+    match(u:User) where u.id in $ids
     match(p:Post) where p.id = $postId
     create(n:Notification{id: apoc.create.uuid(), read: false, createdAt: $createdAt})
     merge (n)-[:NOTIFIED]->(u)
     merge (p)-[:NOTIFIED]->(n)
     `
-  await session.run(cypher, { slugs, createdAt, postId })
+  await session.run(cypher, { ids, createdAt, postId })
   session.close()
 
   return post
