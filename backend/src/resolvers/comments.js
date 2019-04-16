9bc0c0f92cb95b6b0b7a86eac090ba98d76c1c31
Matt Rider <mattwr18@gmail.com>
2019-04-16 20:31:34 -0300

    Add tests, create comments only when authenticated

--- backend/src/resolvers/comments.js
+++ backend/src/resolvers/comments.js
@@ -5,7 +5,7 @@ export default {
     CreateComment: async (object, params, context, resolveInfo) => {
       const { postId } = params
       
-      const result = await neo4jgraphql(object, params, context, resolveInfo, true)
+      const comment = await neo4jgraphql(object, params, context, resolveInfo, true)
       
       const session = context.driver.session()
       const transactionRes = await session.run(`
@@ -13,12 +13,9 @@ export default {
          MERGE (post)<-[:COMMENTS]-(comment)
          RETURN comment {.id, .content}`, {
           postId,
-          commentId: result.id
+          commentId: comment.id
         }
       )
-      const [comment] = transactionRes.records.map(record => {
-        return record.get('comment')
-      })
 
       session.close()
 
