5978adff68b70a757f5ec62c975ede72c96c4de8
Robert Schäfer <git@roschaefer.de>
2019-04-23 19:35:41 +0200

    Sketch a test for a custom image component
    @tansaku @aonomike I hope this clarifies the task.
    
    When you're done, feel free to use this component wherever we use
    `<img>` in `/webapp/`.

---
+++ webapp/components/Image/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <img
+    v-bind="imageProps"
+    :src="imageSrc"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    imageProps: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    imageSrc() {
+      // TODO implement
+      return 'this should be an image source attribute'
+    }
+  }
+}
+</script>
