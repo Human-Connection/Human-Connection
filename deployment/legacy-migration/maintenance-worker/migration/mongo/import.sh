497f77ae10c1ac502cf2715b180ea4944b294746
Robert Schäfer <git@roschaefer.de>
2019-05-01 12:25:28 +0200

    Breakthrough! Use split+indices for performance
    @appinteractive thanks for pointing out `split`. You just saved me some
    days of work to refactor the import statements to use CSV instead of
    JSON files.
    
    @Tirokk when I enter `:schema` in Neo4J web UI, I see the following:
    ```
    :schema
    Indexes
       ON :Badge(id) ONLINE
       ON :Category(id) ONLINE
       ON :Comment(id) ONLINE
       ON :Post(id) ONLINE
       ON :Tag(id) ONLINE
       ON :User(id) ONLINE
    
    No constraints
    ```
    
    So I temporarily removed the unique constraints on `slug` and added
    plain indices on `id` for all relevant node types. We cannot omit the
    `:Label` unfortunately, neo4j does not allow this. So I had to add all
    indices for all known node labels instead.
    
    With indices the import finishes in:
    ```
    Time elapsed: 351 seconds
    ```
    :tada:
    
    @appinteractive when I keep the unique indices on slug, I get an error
    during import that a node with label `:User` and slug `tobias` already
    exists. Ie. we have unqiue constraint violations in our production data.
    
    @mattwr18 @ulfgebhardt @ogerly I started the application on my machine
    on the production data and it turns out that the index page
    http://localhost:3000/ takes way to long. Visiting my profile page at
    http://localhost:3000/profile/5b1693daf850c11207fa6109/robert-schafer
    is fine, though. Even pagination works. When I visit a post page with
    not too many comments, the application is fast enough, too:
    http://localhost:3000/post/5bbf49ebc428ea001c7ca89c/neues-video-format-human-connection-tech-news

--- deployment/legacy-migration/maintenance-worker/migration/mongo/import.sh
+++ deployment/legacy-migration/maintenance-worker/migration/mongo/import.sh
@@ -9,16 +9,17 @@ echo "MONGODB_DATABASE         ${MONGODB_DATABASE}"
 echo "MONGODB_AUTH_DB          ${MONGODB_AUTH_DB}"
 echo "-------------------------------------------------"
 
-[ -z "$SSH_PRIVATE_KEY" ] || create_private_ssh_key_from_env
 
 rm -rf /tmp/mongo-export/*
-mkdir -p /tmp/mongo-export
+mkdir -p /tmp/mongo-export/
 
 ssh -4 -M -S my-ctrl-socket -fnNT -L 27018:localhost:27017 -l ${SSH_USERNAME} ${SSH_HOST}
 
 for collection in "categories" "badges" "users" "contributions" "comments" "follows" "shouts"
 do
-  mongoexport --host localhost -d ${MONGODB_DATABASE} --port 27018 --username ${MONGODB_USERNAME} --password ${MONGODB_PASSWORD} --authenticationDatabase ${MONGODB_AUTH_DB} --db ${MONGODB_DATABASE} --collection $collection --out "/tmp/mongo-export/$collection.json"
+  mongoexport --db ${MONGODB_DATABASE} --host localhost -d ${MONGODB_DATABASE} --port 27018 --username ${MONGODB_USERNAME} --password ${MONGODB_PASSWORD} --authenticationDatabase ${MONGODB_AUTH_DB} --collection $collection  --collection $collection --out "/tmp/mongo-export/$collection.json"
+  mkdir -p /tmp/mongo-export/splits/$collection/
+  split -l 500 /tmp/mongo-export/$collection.json /tmp/mongo-export/splits/$collection/
 done
 
 ssh -S my-ctrl-socket -O check -l ${SSH_USERNAME} ${SSH_HOST}
