4d631c452d09bc5ea44cb3a04aec14c4a917cea7
Matt Rider <mattwr18@gmail.com>
2019-05-04 17:42:42 -0300

    Add more unit tests, add back postId deletion

--- backend/src/resolvers/comments.spec.js
+++ backend/src/resolvers/comments.spec.js
@@ -6,6 +6,8 @@ const factory = Factory()
 let client
 let createCommentVariables
 let createPostVariables
+let createCommentVariablesSansPostId
+let createCommentVariablesWithNonExistentPost
 
 beforeEach(async () => {
   await factory.create('User', {
@@ -34,6 +36,13 @@ describe('CreateComment', () => {
     }
   }
   `
+  const commentQueryForPostId = `
+    query($content: String) {
+      Comment(content: $content) {
+        postId
+      }
+    }
+  `
   describe('unauthenticated', () => {
     it('throws authorization error', async () => {
       createCommentVariables = {
@@ -54,6 +63,12 @@ describe('CreateComment', () => {
         postId: 'p1',
         content: 'I\'m authorised to comment'
       }
+      createPostVariables = {
+        id: 'p1',
+        title: 'post to comment on',
+        content: 'please comment on me'
+      }
+      await client.request(createPostMutation, createPostVariables)
     })
 
     it('creates a comment', async () => {
@@ -67,12 +82,6 @@ describe('CreateComment', () => {
     })
 
     it('assigns the authenticated user as author', async () => {
-      createPostVariables = {
-        id: 'p1',
-        title: 'post to comment on',
-        content: 'please comment on me'
-      }
-      await client.request(createPostMutation, createPostVariables)
       await client.request(createCommentMutation, createCommentVariables)
 
       const { User } = await client.request(`{
@@ -86,7 +95,7 @@ describe('CreateComment', () => {
       expect(User).toEqual([ { comments: [ { content: 'I\'m authorised to comment' } ] } ])
     })
 
-    it('throw an error if an empty string is sent as content', async () => {
+    it('throw an error if an empty string is sent from the editor as content', async () => {
       createCommentVariables = {
         postId: 'p1',
         content: '<p></p>'
@@ -96,7 +105,7 @@ describe('CreateComment', () => {
         .rejects.toThrow('Comment must be at least 1 character long!')
     })
 
-    it('throws an error if a comment does not contain a single character', async () => {
+    it('throws an error if a comment sent from the editor does not contain a single character', async () => {
       createCommentVariables = {
         postId: 'p1',
         content: '<p> </p>'
@@ -104,6 +113,66 @@ describe('CreateComment', () => {
 
       await expect(client.request(createCommentMutation, createCommentVariables))
         .rejects.toThrow('Comment must be at least 1 character long!')
+    })
+
+    it('throws an error if postId is sent as an empty string', async () => {
+      createCommentVariables = {
+        postId: 'p1',
+        content: ''
+      }
+
+      await expect(client.request(createCommentMutation, createCommentVariables))
+        .rejects.toThrow('Comment must be at least 1 character long!')
+    })
+
+    it('throws an error if content is sent as an string of empty characters', async () => {
+      createCommentVariables = {
+        postId: 'p1',
+        content: '    '
+      }
+
+      await expect(client.request(createCommentMutation, createCommentVariables))
+        .rejects.toThrow('Comment must be at least 1 character long!')
+    })
+
+    it('throws an error if postId is sent as an empty string', async () => {
+      createCommentVariablesSansPostId = {
+        postId: '',
+        content: 'this comment should not be created'
+      }
+
+      await expect(client.request(createCommentMutation, createCommentVariablesSansPostId))
+        .rejects.toThrow('Comment cannot be created without a post!')
+    })
+
+    it('throws an error if postId is sent as an string of empty characters', async () => {
+      createCommentVariablesSansPostId = {
+        postId: '   ',
+        content: 'this comment should not be created'
+      }
+
+      await expect(client.request(createCommentMutation, createCommentVariablesSansPostId))
+        .rejects.toThrow('Comment cannot be created without a post!')
+    })
+
+    it('throws an error if the post does not exist in the database', async () => {
+      createCommentVariablesWithNonExistentPost = {
+        postId: 'p2',
+        content: 'comment should not be created cause the post doesn\'t exist'
+      }
+
+      await expect(client.request(createCommentMutation, createCommentVariablesWithNonExistentPost))
+        .rejects.toThrow('Comment cannot be created without a post!')
+    })
+
+    it('does not create the comment with the postId as an attribute', async () => {
+      const commentQueryVariablesByContent = {
+        content: 'I\'m authorised to comment'
+      }
+
+      await client.request(createCommentMutation, createCommentVariables)
+      const { Comment } = await client.request(commentQueryForPostId, commentQueryVariablesByContent)
+      expect(Comment).toEqual([{ postId: null }])
     })
   })
 })
