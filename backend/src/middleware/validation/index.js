81a26e14ffbbc680f2e49c46903dd799025539e8
Robert Schäfer <git@roschaefer.de>
2019-05-10 17:34:05 +0200

    Fix test case, move spec to the right location
    @ulfgebhardt: The reason why the test case was failing is pretty
    obvious. You forgot to create a user that you want to update. If there
    is no user to update, then you get an empty response.
    
    @ulfgebhardt: I moved the spec also in the right directory. You're
    testing resolvers, so that's where I moved the `.spec` file.

--- backend/src/middleware/validation/index.js
+++ backend/src/middleware/validation/index.js
@@ -4,6 +4,7 @@ const USERNAME_MIN_LENGTH = 3
 
 const validateUsername = async (resolve, root, args, context, info) => {
   if (args.name && args.name.length >= USERNAME_MIN_LENGTH) {
+    /* eslint-disable-next-line no-return-await */
     return await resolve(root, args, context, info)
   } else {
     throw new UserInputError(`Username must be at least ${USERNAME_MIN_LENGTH} characters long!`)
@@ -14,6 +15,7 @@ const validateUrl = async (resolve, root, args, context, info) => {
   const { url } = args
   const isValid = url.match(/^(?:https?:\/\/)(?:[^@\n])?(?:www\.)?([^:/\n?]+)/g)
   if (isValid) {
+    /* eslint-disable-next-line no-return-await */
     return await resolve(root, args, context, info)
   } else {
     throw new UserInputError('Input is not a URL')
@@ -22,9 +24,8 @@ const validateUrl = async (resolve, root, args, context, info) => {
 
 export default {
   Mutation: {
-    // CreateUser: validateUsername,
+    CreateUser: validateUsername,
     UpdateUser: validateUsername,
     CreateSocialMedia: validateUrl
   }
 }
-
