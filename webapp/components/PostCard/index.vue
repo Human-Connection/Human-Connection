a7fb3f00953291e11fbbe63cb3e98bcbfdffc087
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2019-05-18 12:36:51 +0200

    linting

--- webapp/components/PostCard/index.vue
+++ webapp/components/PostCard/index.vue
@@ -1,26 +1,43 @@
 <template>
-  <ds-card :image="post.image" :class="{'post-card': true, 'disabled-content': post.disabled}">
+  <ds-card
+    :image="post.image"
+    :class="{'post-card': true, 'disabled-content': post.disabled}"
+  >
     <!-- Post Link Target -->
     <nuxt-link
       class="post-link"
       :to="{ name: 'post-id-slug', params: { id: post.id, slug: post.slug } }"
-    >{{ post.title }}</nuxt-link>
-    <ds-space margin-bottom="small"/>
+    >
+      {{ post.title }}
+    </nuxt-link>
+    <ds-space margin-bottom="small" />
     <!-- Username, Image & Date of Post -->
     <div>
       <no-ssr>
-        <hc-user :user="post.author" :trunc="35" :date-time="post.createdAt"/>
+        <hc-user
+          :user="post.author"
+          :trunc="35"
+          :date-time="post.createdAt"
+        />
       </no-ssr>
-      <hc-ribbon :text="$t('post.name')"/>
+      <hc-ribbon :text="$t('post.name')" />
     </div>
-    <ds-space margin-bottom="small"/>
+    <ds-space margin-bottom="small" />
     <!-- Post Title -->
-    <ds-heading tag="h3" no-margin>{{ post.title }}</ds-heading>
-    <ds-space margin-bottom="small"/>
+    <ds-heading
+      tag="h3"
+      no-margin
+    >
+      {{ post.title }}
+    </ds-heading>
+    <ds-space margin-bottom="small" />
     <!-- Post Content Excerpt -->
     <!-- eslint-disable vue/no-v-html -->
     <!-- TODO: replace editor content with tiptap render view -->
-    <div class="hc-editor-content" v-html="excerpt"/>
+    <div
+      class="hc-editor-content"
+      v-html="excerpt"
+    />
     <!-- eslint-enable vue/no-v-html -->
     <!-- Footer o the Post -->
     <template slot="footer">
@@ -37,17 +54,21 @@
         <div style="display: inline-block; float: right">
           <!-- Shouts Count -->
           <span :style="{ opacity: post.shoutedCount ? 1 : .5 }">
-            <ds-icon name="bullhorn"/>
+            <ds-icon name="bullhorn" />
             <small>{{ post.shoutedCount }}</small>
           </span>
           &nbsp;
           <!-- Comments Count -->
           <span :style="{ opacity: post.commentsCount ? 1 : .5 }">
-            <ds-icon name="comments"/>
+            <ds-icon name="comments" />
             <small>{{ post.commentsCount }}</small>
           </span>
           <!-- Menu -->
-          <content-menu resource-type="contribution" :resource="post" :is-owner="isAuthor"/>
+          <content-menu
+            resource-type="contribution"
+            :resource="post"
+            :is-owner="isAuthor"
+          />
         </div>
       </no-ssr>
     </template>
