341baa5e3b0abe0ae4d8fda57846a04d58070950
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2019-05-18 15:43:26 +0200

    fixed lint rules

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    'jest/globals': true
+    jest: true
   },
   parserOptions: {
     parser: 'babel-eslint'
@@ -15,8 +15,8 @@ module.exports = {
     'jest'
   ],
   rules: {
-    'indent': [ 'error', 2 ],
-    'quotes': [ "error", "single"],
+    //'indent': [ 'error', 2 ],
+    //'quotes': [ "error", "single"],
     // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-console': ['error'],
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
