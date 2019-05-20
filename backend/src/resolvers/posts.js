2b03b515e23472a592cd4c25992ab3801b82b774
Robert Schäfer <git@roschaefer.de>
2019-05-21 01:52:18 +0200

    Implement image upload for posts

--- backend/src/resolvers/posts.js
+++ backend/src/resolvers/posts.js
@@ -1,8 +1,15 @@
 import { neo4jgraphql } from 'neo4j-graphql-js'
+import fileUpload from './fileUpload'
 
 export default {
   Mutation: {
+    UpdatePost: async (object, params, context, resolveInfo) => {
+      params = await fileUpload(params, { file: 'imageUpload', url: 'image'})
+      return neo4jgraphql(object, params, context, resolveInfo, false)
+    },
+
     CreatePost: async (object, params, context, resolveInfo) => {
+      params = await fileUpload(params, { file: 'imageUpload', url: 'image'})
       const result = await neo4jgraphql(object, params, context, resolveInfo, false)
 
       const session = context.driver.session()
