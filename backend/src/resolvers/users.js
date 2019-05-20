d624a3f232efe86e21de4582716bc305183c58d9
Robert Schäfer <git@roschaefer.de>
2019-05-21 00:39:26 +0200

    Write tests for fileUpload

--- backend/src/resolvers/users.js
+++ backend/src/resolvers/users.js
@@ -1,29 +1,15 @@
 import { neo4jgraphql } from 'neo4j-graphql-js'
-import { createWriteStream } from 'fs'
-
-
-const storeUpload = ({ stream, fileLocation}) =>
-  new Promise((resolve, reject) =>
-    stream
-    .pipe(createWriteStream(`public${fileLocation}`))
-    .on("finish", () => resolve())
-    .on("error", reject)
-  );
+import fileUpload from './fileUpload'
 
 export default {
   Mutation: {
     UpdateUser: async (object, params, context, resolveInfo) => {
-      const { avatarUpload } = params
-
-      if (avatarUpload) {
-        const { stream, filename } = await avatarUpload ;
-        const fileLocation = `/uploads/${filename}`
-        await storeUpload({ stream, fileLocation });
-        delete params.avatarUpload
-
-        params.avatar = fileLocation
-      }
+      params = await fileUpload(params, { file: 'avatarUpload', url: 'avatar'})
+      return await neo4jgraphql(object, params, context, resolveInfo, false)
+    },
+    CreateUser: async (object, params, context, resolveInfo) => {
+      params = await fileUpload(params, { file: 'avatarUpload', url: 'avatar'})
       return await neo4jgraphql(object, params, context, resolveInfo, false)
     }
-  },
+  }
 };
