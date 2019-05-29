968578f10a3ad7e47b78e039aba6888b6f62d554
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2019-05-29 17:30:46 +0200

    delete cql files

--- deployment/legacy-migration/maintenance-worker/migration/neo4j/import.sh
+++ deployment/legacy-migration/maintenance-worker/migration/neo4j/import.sh
@@ -6,6 +6,12 @@ set -o allexport
 source $(dirname "$0")/.env
 set +o allexport
 
+# Delete collection function defintion
+function delete_collection () {
+  echo "Delete $1"
+  "${IMPORT_CYPHERSHELL_BIN}" -u ${NEO4J_USERNAME} -p ${NEO4J_PASSWORD} < $(dirname "$0")/$1_delete.cql > /dev/null
+}
+
 # Import collection function defintion
 function import_collection () {
   for chunk in ${IMPORT_PATH}splits/$1/*
@@ -22,7 +28,25 @@ SECONDS=0
 
 # Delete all Neo4J Database content
 echo "Deleting Database Contents"
-"${IMPORT_CYPHERSHELL_BIN}" -u ${NEO4J_USERNAME} -p ${NEO4J_PASSWORD} < $(dirname "$0")/_delete_all.cql > /dev/null
+delete_collection "badges"
+delete_collection "categories"
+delete_collection "users"
+delete_collection "follows"
+delete_collection "contributions"
+delete_collection "shouts"
+delete_collection "comments"
+
+#delete_collection "emotions"
+#delete_collection "invites"
+#delete_collection "notifications"
+#delete_collection "organizations"
+#delete_collection "pages"
+#delete_collection "projects"
+#delete_collection "settings"
+#delete_collection "status"
+#delete_collection "systemnotifications"
+#delete_collection "userscandos"
+#delete_collection "usersettings"
 echo "DONE"
 
 # Import Data
@@ -34,6 +58,7 @@ import_collection "follows"
 import_collection "contributions"
 import_collection "shouts"
 import_collection "comments"
+
 #import_collection "emotions"
 #import_collection "invites"
 #import_collection "notifications"
