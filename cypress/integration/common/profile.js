c6cadc60c378d8e1bafec42a33e29e78ce487a16
Matt Rider <mattwr18@gmail.com>
2019-06-03 21:23:11 -0300

    Remove semicolons added with .vscode linting, unused library

--- cypress/integration/common/profile.js
+++ cypress/integration/common/profile.js
@@ -1,10 +1,10 @@
-import { When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { When, Then } from 'cypress-cucumber-preprocessor/steps'
 
 /* global cy */
 
 When('I visit my profile page', () => {
   cy.openPage('profile/peter-pan')
-});
+})
 
 Then('I should be able to change my profile picture', () => {
   const avatarUpload = 'onourjourney.png'
@@ -13,24 +13,24 @@ Then('I should be able to change my profile picture', () => {
     cy.get('#customdropzone').upload(
       { fileContent, fileName: avatarUpload, mimeType: 'image/png' },
       { subjectType: 'drag-n-drop' }
-    );
-  });
+    )
+  })
   cy.get('.profile-avatar img')
     .should('have.attr', 'src')
     .and('contains', 'onourjourney')
   cy.contains('.iziToast-message', 'Upload successful').should(
     'have.length',
     1
-  );
-});
+  )
+})
 
 When("I visit another user's profile page", () => {
   cy.openPage('profile/peter-pan')
-});
+})
 
 Then('I cannot upload a picture', () => {
   cy.get('.ds-card-content')
     .children()
     .should('not.have.id', 'customdropzone')
     .should('have.class', 'ds-avatar')
-});
+})
