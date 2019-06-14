23185fa29ec38ac45f8d91ff5c5de37a6b55dc2b
Wolfgang Huß <wolle.huss@pjannto.com>
2019-06-14 20:25:25 +0200

    Fix a problem in PostMutations.js which came in through merging the master

--- webapp/graphql/PostMutations.js
+++ webapp/graphql/PostMutations.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 
 export default () => {
   return {
-    CreatePost: gql(`
+    CreatePost: gql`
       mutation($title: String!, $content: String!, $language: String) {
         CreatePost(title: $title, content: $content, language: $language) {
           id
@@ -13,8 +13,8 @@ export default () => {
           language
         }
       }
-    `),
-    UpdatePost: gql(`
+    `,
+    UpdatePost: gql`
       mutation($id: ID!, $title: String!, $content: String!, $language: String) {
         UpdatePost(id: $id, title: $title, content: $content, language: $language) {
           id
