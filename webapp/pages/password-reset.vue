7228d68149e10517be360f1f6db3a08285ffd633
Robert Schäfer <git@roschaefer.de>
2019-06-17 12:30:39 +0200

    Write a nice form to reset your password

--- webapp/pages/password-reset.vue
+++ webapp/pages/password-reset.vue
@@ -3,9 +3,26 @@
     <ds-flex>
       <ds-flex-item :width="{ base: '100%' }" centered>
         <ds-space style="text-align: center;" margin-top="small" margin-bottom="xxx-small" centered>
-          <ds-heading tag="h3">
-            {{ $t('password-reset.title') }}
-          </ds-heading>
+          <ds-card class="password-reset-card">
+            <ds-space margin="large">
+              <form>
+                <ds-input
+                  :placeholder="$t('login.email')"
+                  type="email"
+                  name="email"
+                  icon="envelope"
+                />
+                <ds-space margin-botton="large">
+                  <ds-text>
+                    {{ $t('password-reset.form.description') }}
+                  </ds-text>
+                </ds-space>
+                <ds-button primary fullwidth name="submit" type="submit" icon="sign-in">
+                  {{ $t('password-reset.form.submit') }}
+                </ds-button>
+              </form>
+            </ds-space>
+          </ds-card>
         </ds-space>
       </ds-flex-item>
     </ds-flex>
