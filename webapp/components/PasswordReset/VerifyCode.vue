0da7b906bde543bc5552fbca27ac6bac2fe0419d
Robert Schäfer <git@roschaefer.de>
2019-06-17 21:11:10 +0200

    Start with verify code component

--- webapp/components/PasswordReset/VerifyCode.vue
+++ webapp/components/PasswordReset/VerifyCode.vue
@@ -1,9 +1,29 @@
 <template>
   <ds-card class="verify-code">
     <ds-space margin="large">
-      <h1>
-        {{ $t('verify-code.form.description') }}
-      </h1>
+      <ds-form v-model="formData" :schema="formSchema" @submit="handleSubmit">
+        <ds-input
+          :placeholder="$t('verify-code.form.input')"
+          model="token"
+          name="token"
+          icon="question-circle"
+        />
+        <ds-space margin-botton="large">
+          <ds-text>
+            {{ $t('verify-code.form.description') }}
+          </ds-text>
+        </ds-space>
+        <ds-button
+          :disabled="disabled"
+          :loading="$apollo.loading"
+          primary
+          fullwidth
+          name="submit"
+          type="submit"
+        >
+          {{ $t('verify-code.form.submit') }}
+        </ds-button>
+      </ds-form>
     </ds-space>
   </ds-card>
 </template>
