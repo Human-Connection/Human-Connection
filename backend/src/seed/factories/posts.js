378b17b66d12e45a8acf7bd32b74f6b7b4934e27
Matt Rider <mattwr18@gmail.com>
2019-06-24 11:03:01 -0300

    Spike use of casual as seed data generator

--- backend/src/seed/factories/posts.js
+++ backend/src/seed/factories/posts.js
@@ -1,19 +1,24 @@
-import faker from 'faker'
+import casual from 'casual'
 import uuid from 'uuid/v4'
 
+casual.define('image', function() {
+  return {
+    url: 'https://loremflickr.com/640/480',
+  }
+})
 export default function(params) {
   const {
     id = uuid(),
     slug = '',
-    title = faker.lorem.sentence(),
+    title = casual.title,
     content = [
-      faker.lorem.sentence(),
-      faker.lorem.sentence(),
-      faker.lorem.sentence(),
-      faker.lorem.sentence(),
-      faker.lorem.sentence(),
+      casual.sentence,
+      casual.sentence,
+      casual.sentence,
+      casual.sentence,
+      casual.sentence,
     ].join('. '),
-    image = faker.image.image(),
+    image = casual.image.url,
     visibility = 'public',
     deleted = false,
   } = params
