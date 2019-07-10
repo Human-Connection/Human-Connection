a87330ef9b5617622a161ef6689b2760fc4a1488
Robert Schäfer <git@roschaefer.de>
2019-07-10 10:33:21 +0200

    Update maintenance docker image on master build

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -4,7 +4,9 @@ docker build --build-arg BUILD_COMMIT=$TRAVIS_COMMIT --target production -t huma
 docker build --build-arg BUILD_COMMIT=$TRAVIS_COMMIT --target production -t humanconnection/nitro-web:latest $TRAVIS_BUILD_DIR/webapp
 docker build --build-arg BUILD_COMMIT=$TRAVIS_COMMIT -t humanconnection/neo4j:latest $TRAVIS_BUILD_DIR/neo4j
 docker build -t humanconnection/maintenance-worker:latest $TRAVIS_BUILD_DIR/deployment/legacy-migration/maintenance-worker
+docker build -t humanconnection/maintenance:latest $TRAVIS_BUILD_DIR/deployment/human-connection/maintenance
 docker push humanconnection/nitro-backend:latest
 docker push humanconnection/nitro-web:latest
 docker push humanconnection/neo4j:latest
 docker push humanconnection/maintenance-worker:latest
+docker push humanconnection/maintenance:latest
