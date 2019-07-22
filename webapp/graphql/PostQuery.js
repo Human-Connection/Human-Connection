69de04f1ebc34285ea98083422ecc58be77e7a90
Matt Rider <mattwr18@gmail.com>
2019-07-22 05:52:01 -0300

    Remove extra parantheses

--- webapp/graphql/PostQuery.js
+++ webapp/graphql/PostQuery.js
@@ -78,7 +78,7 @@ export default i18n => {
 
 export const filterPosts = i18n => {
   const lang = i18n.locale().toUpperCase()
-  return gql(`
+  return gql`
   query Post($filter: _PostFilter, $first: Int, $offset: Int, $orderBy: [_PostOrdering]) {
     Post(filter: $filter, first: $first, offset: $offset, orderBy: $orderBy) {
       id
