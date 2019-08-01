3d224512b1ab183d51fdd60db06c22902d54ef8a
Robert Schäfer <git@roschaefer.de>
2019-08-02 00:19:28 +0200

    Warn about non-functional errorHandler hook
    See: https://github.com/nuxt-community/apollo-module/pull/218#issuecomment-506939244

--- webapp/plugins/apollo-error-handler.js
+++ webapp/plugins/apollo-error-handler.js
@@ -1,5 +1,10 @@
 export default (error, context) => {
   /* eslint-disable-next-line no-console */
+
+  // TODO: I added this code without ever reaching it!
+  // See: https://github.com/nuxt-community/apollo-module/pull/218#issuecomment-506939244
+  // Maybe this `errorHandler` callback is never being called?
+
   console.log(
     '%cError',
     'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;',
