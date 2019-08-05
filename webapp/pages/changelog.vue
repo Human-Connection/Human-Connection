49acea976795adcca95d54769d210f618f29ac1c
Robert Schäfer <git@roschaefer.de>
2019-08-05 20:45:40 +0200

    Improve changelog and translations
    * remove unnecessary markup
    * remove manual translations of dates (we have a helper for that)
    * fix typos and English translations

--- webapp/pages/changelog.vue
+++ webapp/pages/changelog.vue
@@ -5,79 +5,71 @@
     </ds-space>
 
     <ds-container>
-      <ds-space margin-top="large">
-        <ds-text>{{ $t('site.changelog') }}</ds-text>
-
-        <p>
-          <strong>{{ $t('changelog.text33') }}</strong>
-        </p>
-        <br />
-        <p>
-          <strong>{{ $t('changelog.text32') }}</strong>
-        </p>
-        <ul>
-          <li>{{ $t('changelog.text31') }}</li>
-          <li>{{ $t('changelog.text30') }}</li>
-          <li>{{ $t('changelog.text29') }}</li>
-          <li>{{ $t('changelog.text28') }}</li>
-          <li>{{ $t('changelog.text27') }}</li>
-          <li>{{ $t('changelog.text26') }}</li>
-        </ul>
-        <p>
-          <strong>{{ $t('changelog.text25') }}</strong>
-        </p>
-        <ul>
-          <li>{{ $t('changelog.text24') }}</li>
-          <li>{{ $t('changelog.text23') }}</li>
-        </ul>
-        <p>
-          <strong>{{ $t('changelog.text22') }}</strong>
-        </p>
-        <ul>
-          <li>{{ $t('changelog.text21') }}</li>
-          <li>{{ $t('changelog.text20') }}</li>
-          <li>{{ $t('changelog.text19') }}</li>
-          <li>{{ $t('changelog.text18') }}</li>
-          <li>{{ $t('changelog.text17') }}</li>
-          <li>{{ $t('changelog.text16') }}</li>
-          <li>{{ $t('changelog.text15') }}</li>
-        </ul>
-        <p>
-          <strong>{{ $t('changelog.text14') }}</strong>
-        </p>
-        <ul>
-          <li>{{ $t('changelog.text13') }}</li>
-          <li>{{ $t('changelog.text12') }}</li>
-          <li>{{ $t('changelog.text11') }}</li>
-        </ul>
-        <p>
-          <strong>{{ $t('changelog.text10') }}</strong>
-        </p>
-        <ul>
-          <li>{{ $t('changelog.text9') }}</li>
-          <li>{{ $t('changelog.text8') }}</li>
-          <li>{{ $t('changelog.text7') }}</li>
-          <li>{{ $t('changelog.text6') }}</li>
-          <li>{{ $t('changelog.text5') }}</li>
-          <li>{{ $t('changelog.text4') }}</li>
-        </ul>
-        <p>
-          <strong>{{ $t('changelog.text3') }}</strong>
-        </p>
-        <ul>
-          <li>
-            {{ $t('changelog.text2') }}
-            <a
-              class="hc-editor-link-blot"
-              href="https://www.youtube.com/watch?v=8j2H0vnQEoQ"
-              target="_blank"
-            >
-              {{ $t('changelog.text1') }}
-            </a>
-            {{ $t('changelog.text0') }}
-          </li>
-        </ul>
-      </ds-space>
+      <strong>
+        {{ Date.parse('2019-03') | date('MMMM yyyy') }}
+      </strong>
+      <ul>
+        <li>{{ $t('changelog.24') }}</li>
+        <li>{{ $t('changelog.23') }}</li>
+        <li>{{ $t('changelog.22') }}</li>
+        <li>{{ $t('changelog.21') }}</li>
+        <li>{{ $t('changelog.20') }}</li>
+      </ul>
+      <strong>
+        {{ Date.parse('2019-02') | date('MMMM yyyy') }}
+      </strong>
+      <ul>
+        <li>{{ $t('changelog.19') }}</li>
+        <li>{{ $t('changelog.18') }}</li>
+        <li>{{ $t('changelog.17') }}</li>
+      </ul>
+      <strong>
+        {{ Date.parse('2019-01') | date('MMMM yyyy') }}
+      </strong>
+      <ul>
+        <li>{{ $t('changelog.16') }}</li>
+        <li>{{ $t('changelog.15') }}</li>
+        <li>{{ $t('changelog.14') }}</li>
+        <li>{{ $t('changelog.13') }}</li>
+        <li>{{ $t('changelog.12') }}</li>
+        <li>{{ $t('changelog.11') }}</li>
+        <li>{{ $t('changelog.10') }}</li>
+      </ul>
+      <strong>
+        {{ Date.parse('2018-12') | date('MMMM yyyy') }}
+      </strong>
+      <ul>
+        <li>{{ $t('changelog.9') }}</li>
+        <li>{{ $t('changelog.8') }}</li>
+        <li>{{ $t('changelog.7') }}</li>
+      </ul>
+      <strong>
+        {{ Date.parse('2018-11') | date('MMMM yyyy') }}
+      </strong>
+      <ul>
+        <li>{{ $t('changelog.6') }}</li>
+        <li>{{ $t('changelog.5') }}</li>
+        <li>{{ $t('changelog.4') }}</li>
+        <li>{{ $t('changelog.3') }}</li>
+        <li>{{ $t('changelog.2') }}</li>
+        <li>{{ $t('changelog.1') }}</li>
+      </ul>
+      <strong>
+        {{ Date.parse('2018-10') | date('MMMM yyyy') }}
+      </strong>
+      <ul>
+        <li>
+          {{ $t('changelog.0') }}
+          <br />
+          <a
+            class="hc-editor-link-blot"
+            href="https://www.youtube.com/watch?v=8j2H0vnQEoQ"
+            target="_blank"
+          >
+            https://www.youtube.com/watch?v=8j2H0vnQEoQ
+          </a>
+        </li>
+      </ul>
     </ds-container>
   </div>
 </template>
