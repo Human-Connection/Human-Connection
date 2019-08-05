56100010ec2f3d115447ebda8f39e4f57a702995
Robert Schäfer <git@roschaefer.de>
2019-08-05 22:36:56 +0200

    DRY template, restructure translations

--- webapp/pages/code-of-conduct.vue
+++ webapp/pages/code-of-conduct.vue
@@ -2,85 +2,36 @@
   <div>
     <ds-space>
       <ds-heading tag="h2">{{ $t('site.code-of-conduct') }}</ds-heading>
+      <p>{{ $t('code-of-conduct.subheader') }}</p>
     </ds-space>
     <ds-container>
-      <ds-space margin-top="large">
-        <ds-text>{{ $t('site.code-of-conduct') }}</ds-text>
-      </ds-space>
 
-      <p>{{ $t('code-of-conduct.text0') }}</p>
       <br />
+      <div v-for="section in sections" :key="section">
+      <strong>{{ $t(`code-of-conduct.${section}.title`) }}</strong>
+      <p>{{ $t(`code-of-conduct.${section}.description`) }}</p>
+      </div>
+
+      <br />
+
+      <div v-for="section in listSections" :key="section.key">
+        <strong>{{ $t(`code-of-conduct.${section.key}.title`) }}</strong>
+        <p>{{ $t(`code-of-conduct.${section.key}.description`) }}</p>
+        <ul>
+          <li v-for="i in section.items">
+            {{ $t(`code-of-conduct.${section.key}.list.${i}`) }}
+          </li>
+        </ul>
+      </div>
+
       <p>
-        <strong>{{ $t('code-of-conduct.text1') }}</strong>
-      </p>
-      <br />
-      <p>{{ $t('code-of-conduct.text2') }}</p>
-      <br />
-      <p>{{ $t('code-of-conduct.text3') }}</p>
-      <br />
-      <p>
-        <strong>{{ $t('code-of-conduct.text4') }}</strong>
-      </p>
-      <br />
-      <p>{{ $t('code-of-conduct.text5') }}</p>
-      <br />
-      <p>{{ $t('code-of-conduct.text6') }}</p>
-      <br />
-      <p>
-        <strong>{{ $t('code-of-conduct.text7') }}</strong>
-      </p>
-      <br />
-      <p>{{ $t('code-of-conduct.text8') }}</p>
-      <ul>
-        <li>{{ $t('code-of-conduct.text9') }}</li>
-        <li>{{ $t('code-of-conduct.text10') }}</li>
-        <li>{{ $t('code-of-conduct.text11') }}</li>
-        <li>{{ $t('code-of-conduct.text12') }}</li>
-      </ul>
-      <p>
-        <strong>{{ $t('code-of-conduct.text13') }}</strong>
-      </p>
-      <br />
-      <p>{{ $t('code-of-conduct.text14') }}</p>
-      <ul>
-        <li>{{ $t('code-of-conduct.text15') }}</li>
-        <li>{{ $t('code-of-conduct.text16') }}</li>
-        <li>{{ $t('code-of-conduct.text17') }}</li>
-        <li>{{ $t('code-of-conduct.text18') }}</li>
-        <li>{{ $t('code-of-conduct.text19') }}</li>
-        <li>{{ $t('code-of-conduct.text20') }}</li>
-        <li v-html="$t('code-of-conduct.text21')"></li>
-        <li>{{ $t('code-of-conduct.text22') }}</li>
-      </ul>
-      <p>
-        <strong>{{ $t('code-of-conduct.text23') }}</strong>
-      </p>
-      <br />
-      <p>{{ $t('code-of-conduct.text24') }}</p>
-      <ul>
-        <li>{{ $t('code-of-conduct.text25') }}</li>
-        <li>{{ $t('code-of-conduct.text26') }}</li>
-        <li>{{ $t('code-of-conduct.text27') }}</li>
-        <li>{{ $t('code-of-conduct.text28') }}</li>
-        <li>{{ $t('code-of-conduct.text29') }}</li>
-        <li>{{ $t('code-of-conduct.text30') }}</li>
-        <li>{{ $t('code-of-conduct.text31') }}</li>
-      </ul>
-      <p>{{ $t('code-of-conduct.text32') }}</p>
-      <br />
-      <p>
-        <strong>{{ $t('code-of-conduct.text33') }}</strong>
-      </p>
-      <br />
-      <p>{{ $t('code-of-conduct.text34') }}</p>
-      <br />
-      <p>
+        {{ $t('code-of-conduct.get-help') }}
         <a class="hc-editor-link-blot" href="moderation@human-connection.org" target="_blank">
           moderation@human-connection.org
         </a>
       </p>
       <br />
-      <p>{{ $t('code-of-conduct.text35') }}</p>
+
     </ds-container>
   </div>
 </template>
@@ -92,5 +43,22 @@ export default {
       title: this.$t('site.code-of-conduct'),
     }
   },
+  data() {
+    return {
+      sections: ['preamble', 'purpose', ],
+      listSections: [
+        {
+          key: 'expected-behaviour',
+          items: [...Array(4).keys()]
+        },{
+          key: 'unacceptable-behaviour',
+          items: [...Array(8).keys()]
+        },{
+          key: 'consequences',
+          items: [...Array(9).keys()]
+        }
+      ]
+    }
+  }
 }
 </script>
