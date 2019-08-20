67ee0fa624b99d750ce62f11e6f4b40e44e621e1
roschaefer <gitkraken@roschaefer.de>
2019-08-20 19:52:04 +0200

    Sketch a backend test for #1187

---
+++ backend/src/schema/resolvers/users/termsAndConditions.spec.js
@@ -0,0 +1,22 @@
+describe('SignupVerification', () => {
+  describe('given a valid version', () => {
+    let version = '1.2.3'
+
+    it.todo('saves the version with the new created user account')
+    it.todo('saves the current datetime in `termsAndConditionsAgreedAt`')
+  })
+
+  describe('given an invalid version string', () => {
+    let version = 'this string does not follow semantic versioning'
+
+    it.todo('rejects')
+  })
+})
+
+
+describe('UpdateUser', () => {
+  describe('given a new agreed version of terms and conditions', () => {
+    it.todo('updates `termsAndConditionsAgreedAt`')
+    it.todo('updates `termsAndConditionsAgreedVersion`')
+  })
+})
