44573b7460d77149f15375da608e762d59872404
Brent Vardy <brent.vardy@challengelogic.net>
2019-09-05 14:17:59 +0100

    refactor page footer to component

--- webapp/layouts/default.vue
+++ webapp/layouts/default.vue
@@ -126,19 +126,7 @@
         <nuxt />
       </div>
     </ds-container>
-    <div id="footer" class="ds-footer">
-      <a href="https://human-connection.org" target="_blank" v-html="$t('site.made')"></a>
-      &nbsp;-&nbsp;
-      <nuxt-link to="/imprint">{{ $t('site.imprint') }}</nuxt-link>
-      &nbsp;‑&nbsp;
-      <nuxt-link to="/terms-and-conditions">{{ $t('site.termsAndConditions') }}</nuxt-link>
-      &nbsp;‑&nbsp;
-      <nuxt-link to="/code-of-conduct">{{ $t('site.code-of-conduct') }}</nuxt-link>
-      &nbsp;‑&nbsp;
-      <nuxt-link to="/data-privacy">{{ $t('site.data-privacy') }}</nuxt-link>
-      &nbsp;‑&nbsp;
-      <nuxt-link to="/changelog">{{ $t('site.changelog') }}</nuxt-link>
-    </div>
+    <page-footer></page-footer>
     <div id="overlay" />
     <client-only>
       <modal />
@@ -157,6 +145,8 @@ import HcAvatar from '~/components/Avatar/Avatar.vue'
 import seo from '~/mixins/seo'
 import FilterPosts from '~/components/FilterPosts/FilterPosts.vue'
 import CategoryQuery from '~/graphql/CategoryQuery.js'
+import PageFooter from '~/components/PageFooter/PageFooter'
+
 export default {
   components: {
     Dropdown,
@@ -166,6 +156,7 @@ export default {
     NotificationMenu,
     HcAvatar,
     FilterPosts,
+    PageFooter,
   },
   mixins: [seo],
   data() {
