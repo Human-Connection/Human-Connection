beb78249f6ef97691302a42708669bda78285cb9
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2019-09-09 10:01:46 +0300

    update follower count on follow/unfollow

--- webapp/components/User/index.vue
+++ webapp/components/User/index.vue
@@ -79,8 +79,8 @@
             <hc-follow-button
               :follow-id="user.id"
               :is-followed="user.followedByCurrentUser"
-              @optimistic="follow => (user.followedByCurrentUser = follow)"
-              @update="follow => (user.followedByCurrentUser = follow)"
+              @optimistic="optimisticFollow"
+              @update="updateFollow"
             />
           </ds-flex-item>
           <ds-flex-item :width="{ base: 1 }">
@@ -133,6 +133,16 @@ export default {
     userName() {
       const { name } = this.user || {}
       return name || this.$t('profile.userAnonym')
+    },
+  },
+  methods: {
+    optimisticFollow(follow) {
+      const inc = follow ? 1 : -1
+      this.user.followedByCurrentUser = follow
+      this.user.followedByCount += inc
+    },
+    updateFollow(follow) {
+      this.user.followedByCurrentUser = follow
     },
   },
 }
