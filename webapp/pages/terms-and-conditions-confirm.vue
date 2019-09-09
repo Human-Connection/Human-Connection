560a1d6d380697e6fbd2678c54dfc475d4a6a1bb
Alexander Friedland <ogerly@users.noreply.github.com>
2019-09-09 15:15:18 +0200

    Update webapp/pages/terms-and-conditions-confirm.vue
    Co-Authored-By: Robert Schäfer <git@roschaefer.de>

--- webapp/pages/terms-and-conditions-confirm.vue
+++ webapp/pages/terms-and-conditions-confirm.vue
@@ -95,7 +95,6 @@ export default {
             this.setCurrentUser({
               ...this.currentUser,
               termsAndConditionsAgreedVersion,
-              termsAndConditionsAgreedAt,
             })
           },
         })
