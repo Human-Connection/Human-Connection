f802eab27df5a5ee7b6a34f088c40c7b292043d6
Alina Beck <alina.beck@mail.com>
2019-09-11 12:44:15 +0100

    copy email text to separate file

--- backend/src/middleware/email/templates/signup.js
+++ backend/src/middleware/email/templates/signup.js
@@ -1,4 +1,9 @@
 import CONFIG from '../../../config'
+import fs from 'fs'
+import path from 'path'
+import mustache from 'mustache'
+
+const signupText = fs.readFileSync(path.join(__dirname, './signup.txt'), 'utf-8')
 
 export const signupTemplate = options => {
   const {
@@ -11,52 +16,11 @@ export const signupTemplate = options => {
   actionUrl.searchParams.set('nonce', nonce)
   actionUrl.searchParams.set('email', email)
 
+  console.log(actionUrl)
+
   return {
     to: email,
     subject,
-    text: `
-Willkommen bei Human Connection! Klick auf diesen Link, um den
-Registrierungsprozess abzuschließen und um ein Benutzerkonto zu erstellen!
-
-${actionUrl}
-
-Alternativ kannst du diesen Code auch kopieren und im Browserfenster einfügen:
-
-${nonce}
-
-Bitte ignoriere diese Mail, falls du dich nicht bei Human Connection angemeldet
-hast. Bei Fragen kontaktiere gerne unseren Support:
-
-${supportUrl}
-
-Danke,
-Das Human Connection Team
-
-
-English Version
-===============
-
-Welcome to Human Connection! Use this link to complete the registration process
-and create a user account:
-
-${actionUrl}
-
-You can also copy+paste this verification nonce in your browser window:
-
-${nonce}
-
-If you did not signed up for Human Connection, please ignore this email or
-contact support if you have questions:
-
-${supportUrl}
-
-Thanks,
-The Human Connection Team
-
-Human Connection gemeinnützige GmbH
-Bahnhofstr. 11
-73235 Weilheim / Teck
-Deutschland
-    `,
+    text: mustache.render(signupText, { actionUrl, nonce, supportUrl })
   }
 }
