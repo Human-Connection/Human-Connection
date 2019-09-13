92df820323453ad6885fc623f70fb76bd4bcd730
Wolfgang Huß <wolle.huss@pjannto.com>
2019-09-13 15:09:33 +0200

    Rename `/admin/tags.vue` to `/admin/hashtags.vue`

--- webapp/pages/admin/hashtags.vue
+++ webapp/pages/admin/hashtags.vue
@@ -1,5 +1,5 @@
 <template>
-  <ds-card :header="$t('admin.tags.name')">
+  <ds-card :header="$t('admin.hashtags.name')">
     <ds-table :data="Tag" :fields="fields" condensed>
       <template slot="index" slot-scope="scope">
         {{ scope.index + 1 }}.
@@ -25,14 +25,14 @@ export default {
   computed: {
     fields() {
       return {
-        index: this.$t('admin.tags.number'),
-        id: this.$t('admin.tags.name'),
+        index: this.$t('admin.hashtags.number'),
+        id: this.$t('admin.hashtags.name'),
         taggedCountUnique: {
-          label: this.$t('admin.tags.tagCountUnique'),
+          label: this.$t('admin.hashtags.tagCountUnique'),
           align: 'right',
         },
         taggedCount: {
-          label: this.$t('admin.tags.tagCount'),
+          label: this.$t('admin.hashtags.tagCount'),
           align: 'right',
         },
       }
