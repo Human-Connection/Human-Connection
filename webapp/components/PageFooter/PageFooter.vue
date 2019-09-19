b761eea7000847b4937886ed7b198c08ef191c71
roschaefer <git@roschaefer.de>
2019-09-19 13:21:51 +0200

    Point the changelog to Github
    The justification for this change is that the releases page on Github
    is more likely to keep up to date than our changelog.
    
    This also means that our changelog is not translated. But I think that's
    fine.

--- webapp/components/PageFooter/PageFooter.vue
+++ webapp/components/PageFooter/PageFooter.vue
@@ -2,23 +2,21 @@
   <div id="footer" class="ds-footer">
     <a href="https://human-connection.org" target="_blank" v-html="$t('site.made')"></a>
     <span>-</span>
-    <a
-      href="https://human-connection.org/impressum/"
-      target="_blank"
-      v-html="$t('site.imprint')"
-    ></a>
+    <a href="https://human-connection.org/impressum/" target="_blank">
+      {{ $t('site.imprint') }}
+    </a>
     <span>-</span>
     <nuxt-link to="/terms-and-conditions">{{ $t('site.termsAndConditions') }}</nuxt-link>
     <span>-</span>
     <nuxt-link to="/code-of-conduct">{{ $t('site.code-of-conduct') }}</nuxt-link>
     <span>-</span>
-    <a
-      href="https://human-connection.org/datenschutz/"
-      target="_blank"
-      v-html="$t('site.data-privacy')"
-    ></a>
+    <a href="https://human-connection.org/datenschutz/" target="_blank">
+      {{ $t('site.data-privacy') }}
+    </a>
     <span>-</span>
-    <nuxt-link to="/changelog">{{ $t('site.changelog') }}</nuxt-link>
+    <a href="https://github.com/Human-Connection/Human-Connection/releases" target="_blank">
+      {{ $t('site.changelog') }}
+    </a>
   </div>
 </template>
 
