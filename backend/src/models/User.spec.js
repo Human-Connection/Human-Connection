4fbabc879fc17b9c00cfc7316207448b1429f913
roschaefer <git@roschaefer.de>
2019-09-20 20:02:51 +0200

    Add tests for User validations

--- backend/src/models/User.spec.js
+++ backend/src/models/User.spec.js
@@ -18,3 +18,44 @@ describe('role', () => {
     )
   })
 })
+
+describe('slug', () => {
+  it('normalizes to lowercase letters', async () => {
+    const user = await instance.create('User', { slug: 'Matt' })
+    await expect(user.toJson()).resolves.toEqual(
+      expect.objectContaining({
+        slug: 'matt',
+      }),
+    )
+  })
+
+  describe('characters', () => {
+    const createUser = attrs => {
+      return instance.create('User', attrs).then(user => user.toJson())
+    }
+
+    it('-', async () => {
+      await expect(createUser({ slug: 'matt-rider' })).resolves.toMatchObject({
+        slug: 'matt-rider',
+      })
+    })
+
+    it('_', async () => {
+      await expect(createUser({ slug: 'matt_rider' })).resolves.toMatchObject({
+        slug: 'matt_rider',
+      })
+    })
+
+    it(' ', async () => {
+      await expect(createUser({ slug: 'matt rider' })).rejects.toThrow(
+        /fails to match the required pattern/,
+      )
+    })
+
+    it('ä', async () => {
+      await expect(createUser({ slug: 'mätt' })).rejects.toThrow(
+        /fails to match the required pattern/,
+      )
+    })
+  })
+})
