2818b63fe91411adedb54e57fa8a6046238591ef
mattwr18 <mattwr18@gmail.com>
2019-10-02 23:42:42 +0200

    Fix lint

--- backend/src/schema/resolvers/fileUpload/index.js
+++ backend/src/schema/resolvers/fileUpload/index.js
@@ -11,16 +11,13 @@ const storeUpload = ({ createReadStream, fileLocation }) =>
   )
 
 export default async function fileUpload(params, { file, url }, uploadCallback = storeUpload) {
-  console.log('params', params)
   const upload = params[file]
-  console.log('upload', upload)
   if (upload) {
     const { createReadStream, filename } = await upload
     const { name } = path.parse(filename)
     const fileLocation = `/uploads/${Date.now()}-${slug(name)}`
     await uploadCallback({ createReadStream, fileLocation })
     delete params[file]
-    console.log('fileLocation', fileLocation)
     params[url] = fileLocation
   }
 
