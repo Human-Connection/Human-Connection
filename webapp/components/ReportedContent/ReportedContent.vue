5ea8e5f775037514874d50c0af8835967e2c6997
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2019-10-29 21:36:23 +0300

    wip: move reported content into own component

--- webapp/components/ReportedContent/ReportedContent.vue
+++ webapp/components/ReportedContent/ReportedContent.vue
@@ -1,7 +1,108 @@
 <template>
-  <div></div>
+  <div class="list-item">
+    <div class="icon-container">
+      <!-- Icon -->
+      <ds-text :class="[!content.resource.disabledBy && 'no-decision']" color="soft">
+        <ds-icon
+          v-if="content.type === 'Post'"
+          v-tooltip="{ content: $t('report.contribution.type'), placement: 'right' }"
+          name="bookmark"
+        />
+        <ds-icon
+          v-else-if="content.type === 'Comment'"
+          v-tooltip="{ content: $t('report.comment.type'), placement: 'right' }"
+          name="comments"
+        />
+        <ds-icon
+          v-else-if="content.type === 'User'"
+          v-tooltip="{ content: $t('report.user.type'), placement: 'right' }"
+          name="user"
+        />
+      </ds-text>
+    </div>
+    <div class="content-container">
+      <!-- reported user or content -->
+      <div v-if="content.type === 'Post' || content.type === 'Comment'">
+        <nuxt-link
+          :to="{
+            name: 'post-id-slug',
+            params: {
+              id: content.type === 'Post' ? content.post.id : content.comment.post.id,
+              slug: content.type === 'Post' ? content.post.slug : content.comment.post.slug,
+            },
+            hash: content.type === 'Comment' ? `#commentId-${content.comment.id}` : undefined,
+          }"
+        >
+          <b v-if="content.type === 'Post'">{{ content.post.title | truncate(100) }}</b>
+          <b v-else>{{ content.comment.contentExcerpt | removeHtml | truncate(100) }}</b>
+        </nuxt-link>
+      </div>
+      <div v-else>
+        <hc-user :user="content.user" :showAvatar="false" :trunc="30" />
+      </div>
+    </div>
+    <div class="author-container">
+      <div class="label">
+        {{ $t('moderation.reports.author') }}
+      </div>
+      <ds-flex v-if="content.contentBelongsToUser">
+        <ds-flex-item width="20px">
+          <ds-icon v-tooltip="{ content: $t('report.author'), placement: 'right' }" name="user" />
+        </ds-flex-item>
+        <ds-flex-item>
+          <hc-user :user="content.contentBelongsToUser" :showAvatar="false" :trunc="30" />
+        </ds-flex-item>
+      </ds-flex>
+      <span v-else>—</span>
+    </div>
+    <div class="decision-container">
+      <!-- disabledBy -->
+      <div class="label">
+        {{ $t('moderation.reports.decision') }}
+      </div>
+      <div v-if="content.resource.disabledBy">
+        {{ $t('moderation.reports.disabledBy') }}
+        <br />
+        <hc-user :user="content.resource.disabledBy" :showAvatar="false" :trunc="30" />
+      </div>
+      <span v-else class="no-decision">{{ $t('moderation.reports.noDecision') }}</span>
+    </div>
+
+    <!-- TODO: add reports list -->
+  </div>
 </template>
 
 <script>
-export default {}
+import HcUser from '~/components/User/User'
+
+export default {
+  name: 'HcReportedContent',
+  components: {
+    HcUser,
+  },
+  props: ['content'],
+  computed: {
+    reportFields() {
+      return {
+        submitter: this.$t('moderation.reports.submitter'),
+        reasonCategory: this.$t('moderation.reports.reasonCategory'),
+        reasonDescription: this.$t('moderation.reports.reasonDescription'),
+        // Wolle createdAt: this.$t('moderation.reports.createdAt'),
+      }
+    },
+    // Wolle delete + language
+    fields() {
+      return {
+        type: ' ',
+        reportedUserContent: ' ',
+        reasonCategory: this.$t('moderation.reports.reasonCategory'),
+        reasonDescription: this.$t('moderation.reports.reasonDescription'),
+        submitter: this.$t('moderation.reports.submitter'),
+        createdAt: this.$t('moderation.reports.createdAt'),
+        disabledBy: this.$t('moderation.reports.disabledBy'),
+        // actions: ' '
+      }
+    },
+  },
+}
 </script>
--- webapp/pages/moderation/index.vue
+++ webapp/pages/moderation/index.vue
@@ -1,139 +1,45 @@
 <template>
   <ds-card space="small">
     <ds-heading tag="h3">{{ $t('moderation.reports.name') }}</ds-heading>
-    <table
+    <div
       v-if="reportedContentStructure && reportedContentStructure.length"
-      class="ds-table ds-table-condensed ds-table-bordered"
+      class="list ds-table ds-table-condensed ds-table-bordered"
       cellspacing="0"
       cellpadding="0"
     >
-      <colgroup><col width="" /></colgroup>
-      <template v-for="content in reportedContentStructure">
-        <thead :key="'thead-' + content.resource.id">
-          <tr valign="top">
-            <th class="ds-table-col ds-table-head-col ds-table-head-col-border">
-              {{ $t('moderation.reports.type') }}
-            </th>
-            <th class="ds-table-col ds-table-head-col ds-table-head-col-border">
-              {{ $t('moderation.reports.content') }}
-            </th>
-            <th class="ds-table-col ds-table-head-col ds-table-head-col-border">
-              {{ $t('moderation.reports.author') }}
-            </th>
-            <th class="ds-table-col ds-table-head-col ds-table-head-col-border">
-              {{ $t('moderation.reports.decision') }}
-            </th>
-          </tr>
-        </thead>
-        <tbody :key="'tbody-' + content.resource.id">
-          <tr valign="top">
-            <td class="ds-table-col ds-table-head-col ds-table-head-col-border">
-              <!-- Icon -->
-              <ds-text :class="[!content.resource.disabledBy && 'no-decision']" color="soft">
-                <ds-icon
-                  v-if="content.type === 'Post'"
-                  v-tooltip="{ content: $t('report.contribution.type'), placement: 'right' }"
-                  name="bookmark"
-                />
-                <ds-icon
-                  v-else-if="content.type === 'Comment'"
-                  v-tooltip="{ content: $t('report.comment.type'), placement: 'right' }"
-                  name="comments"
-                />
-                <ds-icon
-                  v-else-if="content.type === 'User'"
-                  v-tooltip="{ content: $t('report.user.type'), placement: 'right' }"
-                  name="user"
-                />
-              </ds-text>
-            </td>
-            <td class="ds-table-col ds-table-head-col-border">
-              <!-- reported user or content -->
-              <div v-if="content.type === 'Post' || content.type === 'Comment'">
-                <nuxt-link
-                  :to="{
-                    name: 'post-id-slug',
-                    params: {
-                      id: content.type === 'Post' ? content.post.id : content.comment.post.id,
-                      slug: content.type === 'Post' ? content.post.slug : content.comment.post.slug,
-                    },
-                    hash:
-                      content.type === 'Comment' ? `#commentId-${content.comment.id}` : undefined,
-                  }"
-                >
-                  <b v-if="content.type === 'Post'">{{ content.post.title | truncate(100) }}</b>
-                  <b v-else>{{ content.comment.contentExcerpt | removeHtml | truncate(100) }}</b>
-                </nuxt-link>
-              </div>
-              <div v-else>
-                <hc-user :user="content.user" :showAvatar="false" :trunc="30" />
-              </div>
-            </td>
-            <td class="ds-table-col ds-table-head-col-border">
-              <ds-flex v-if="content.contentBelongsToUser">
-                <ds-flex-item width="20px">
-                  <ds-icon
-                    v-tooltip="{ content: $t('report.author'), placement: 'right' }"
-                    name="user"
-                  />
-                </ds-flex-item>
-                <ds-flex-item>
-                  <hc-user :user="content.contentBelongsToUser" :showAvatar="false" :trunc="30" />
-                </ds-flex-item>
-              </ds-flex>
-              <span v-else>—</span>
-            </td>
-            <td class="ds-table-col ds-table-head-col-border">
-              <!-- disabledBy -->
-              <div v-if="content.resource.disabledBy">
-                {{ $t('moderation.reports.disabledBy') }}
-                <br />
-                <hc-user :user="content.resource.disabledBy" :showAvatar="false" :trunc="30" />
-              </div>
-              <span v-else class="no-decision">{{ $t('moderation.reports.noDecision') }}</span>
-            </td>
-          </tr>
-          <tr>
-            <td class="ds-table-col ds-table-head-col-border"></td>
-            <td class="ds-table-col ds-table-head-col-border" colspan="3">
-              <ds-table :data="content.reports" :fields="reportFields" condensed>
-                <!-- submitter -->
-                <template slot="submitter" slot-scope="scope">
-                  <hc-user
-                    :user="scope.row.submitter"
-                    :showAvatar="false"
-                    :trunc="30"
-                    :date-time="scope.row.createdAt"
-                    :positionDatetime="'below'"
-                  />
-                </template>
-                <!-- reasonCategory -->
-                <template slot="reasonCategory" slot-scope="scope">
-                  {{ $t('report.reason.category.options.' + scope.row.reasonCategory) }}
-                </template>
-                <!-- reasonDescription -->
-                <template slot="reasonDescription" slot-scope="scope">
-                  {{ scope.row.reasonDescription.length ? scope.row.reasonDescription : '—' }}
-                </template>
-              </ds-table>
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </table>
+      <div class="list-heading">
+        <div class="ds-table-col ds-table-head-col ds-table-head-col-border">
+          {{ $t('moderation.reports.type') }}
+        </div>
+        <div class="ds-table-col ds-table-head-col ds-table-head-col-border">
+          {{ $t('moderation.reports.content') }}
+        </div>
+        <div class="ds-table-col ds-table-head-col ds-table-head-col-border">
+          {{ $t('moderation.reports.author') }}
+        </div>
+        <div class="ds-table-col ds-table-head-col ds-table-head-col-border">
+          {{ $t('moderation.reports.decision') }}
+        </div>
+      </div>
+      <hc-reported-content
+        v-for="content in reportedContentStructure"
+        :key="`row-${content.resource.id}`"
+        :content="content"
+      />
+    </div>
     <hc-empty v-else icon="alert" :message="$t('moderation.reports.empty')" />
   </ds-card>
 </template>
 
 <script>
 import HcEmpty from '~/components/Empty.vue'
-import HcUser from '~/components/User/User'
+import HcReportedContent from '~/components/ReportedContent/ReportedContent'
 import { reportListQuery } from '~/graphql/Moderation.js'
 
 export default {
   components: {
     HcEmpty,
-    HcUser,
+    HcReportedContent,
   },
   data() {
     return {
@@ -141,29 +47,7 @@ export default {
       reportedContentStructure: [],
     }
   },
-  computed: {
-    reportFields() {
-      return {
-        submitter: this.$t('moderation.reports.submitter'),
-        reasonCategory: this.$t('moderation.reports.reasonCategory'),
-        reasonDescription: this.$t('moderation.reports.reasonDescription'),
-        // Wolle createdAt: this.$t('moderation.reports.createdAt'),
-      }
-    },
-    // Wolle delete + language
-    fields() {
-      return {
-        type: ' ',
-        reportedUserContent: ' ',
-        reasonCategory: this.$t('moderation.reports.reasonCategory'),
-        reasonDescription: this.$t('moderation.reports.reasonDescription'),
-        submitter: this.$t('moderation.reports.submitter'),
-        createdAt: this.$t('moderation.reports.createdAt'),
-        disabledBy: this.$t('moderation.reports.disabledBy'),
-        // actions: ' '
-      }
-    },
-  },
+
   watch: {
     reports: {
       immediate: true,
@@ -216,5 +100,59 @@ export default {
 }
 .no-decision {
   color: $text-color-danger;
+}
+
+.list {
+  // TODO: move styling into reported content
+  .list-heading,
+  .list-item {
+    display: grid;
+    grid-template-columns: 1fr 9fr 4fr 2fr;
+
+    .icon-container,
+    .content-container,
+    .decision-container,
+    .author-container {
+      padding: 8px 4px;
+
+      & > .label {
+        display: none;
+      }
+    }
+
+    & > .reports {
+      grid-column-start: 2;
+      grid-column-end: 5;
+    }
+  }
+}
+
+@media screen and (max-width: 960px) {
+  .list {
+    .list-heading {
+      display: none;
+    }
+
+    .list-item {
+      display: block;
+      position: relative;
+      padding-left: 10px;
+      margin-bottom: 30px;
+
+      .label {
+        display: block;
+        border: none;
+        padding: 8px 0;
+        font-weight: 600;
+      }
+
+      & > .icon-container {
+        position: absolute;
+        top: 0;
+        left: -20px;
+        padding: 8px;
+      }
+    }
+  }
 }
 </style>
