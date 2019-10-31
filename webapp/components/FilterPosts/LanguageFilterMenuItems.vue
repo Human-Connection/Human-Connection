3c8a35d37154efb74be7e2367dc1876c60fccd0c
Moriz Wahl <moriz.wahl@gmx.de>
2019-10-31 14:35:36 +0100

    Language Code As Buttons Instead Of Flag Icon

--- webapp/components/FilterPosts/LanguageFilterMenuItems.vue
+++ webapp/components/FilterPosts/LanguageFilterMenuItems.vue
@@ -32,12 +32,13 @@
       <ds-flex v-for="language in locales" :key="language.code" class="languages-menu">
         <ds-flex class="languages-menu">
           <ds-flex-item width="100%" class="language-menu-item">
-              <ds-button
-		  class="language-buttons"
-		  :primary="filteredLanguagesCodes.includes(language.code)"
-		  @click.stop.prevent="toggleLanguage(language.code)">
-		  {{ language.code.toUpperCase()  }}
-	    </ds-button>
+            <ds-button
+              class="language-buttons"
+              :primary="filteredLanguagesCodes.includes(language.code)"
+              @click.stop.prevent="toggleLanguage(language.code)"
+            >
+              {{ language.code.toUpperCase() }}
+            </ds-button>
             <ds-space margin-bottom="small" />
           </ds-flex-item>
           <ds-flex>
