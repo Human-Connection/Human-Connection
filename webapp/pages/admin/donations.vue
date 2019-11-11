2cd7ee9ac6b00e9e007c520c047336c8e4df921c
Alina Beck <alina.beck@mail.com>
2019-11-11 15:07:26 +0300

    add donations info page to admin pages

---
+++ webapp/pages/admin/donations.vue
@@ -0,0 +1,35 @@
+<template>
+  <ds-card :header="$t('admin.donations.name')">
+    <ds-form v-model="formData" @submit="submit">
+      <ds-input model="goal" :label="$t('admin.donations.goal')" placeholder="15000" icon="money" />
+      <ds-input
+        model="progress"
+        :label="$t('admin.donations.progress')"
+        placeholder="1200"
+        icon="money"
+      />
+      <ds-button primary type="submit" :disabled="!formData.goal || !formData.progress">
+        {{ $t('actions.save') }}
+      </ds-button>
+    </ds-form>
+  </ds-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        goal: '',
+        progress: '',
+      },
+    }
+  },
+  methods: {
+    submit() {
+      // TODO: save form data... somewhere
+      console.log('submitting donations form')
+    },
+  },
+}
+</script>
