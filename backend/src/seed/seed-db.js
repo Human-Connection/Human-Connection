6a98e5759fee9cb7d1460133cff4c443af16f35f
Alina Beck <alina.beck@mail.com>
2019-11-12 13:50:45 +0300

    fix lint errors in backend

--- backend/src/seed/seed-db.js
+++ backend/src/seed/seed-db.js
@@ -923,7 +923,7 @@ const languages = ['de', 'en', 'es', 'fr', 'it', 'pt', 'pl']
         })
       }),
     )
-    
+
     await factory.create('Donations')
     /* eslint-disable-next-line no-console */
     console.log('Seeded Data...')
