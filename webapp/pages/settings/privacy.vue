1625e0705e50ce0c74e8612d9794bcf71bed2f84
Alina Beck <alina.beck@mail.com>
2019-11-13 11:02:53 +0300

    run update mutation when submitting shouts form

--- webapp/pages/settings/privacy.vue
+++ webapp/pages/settings/privacy.vue
@@ -4,30 +4,55 @@
       <input id="allow-shouts" type="checkbox" v-model="shoutsAllowed" />
       <label for="allow-shouts">{{ $t('settings.privacy.make-shouts-public') }}</label>
     </ds-space>
-    <ds-button primary @click="submit">{{ $t('actions.save') }}</ds-button>
+    <ds-button primary @click="submit" :disabled="disabled">{{ $t('actions.save') }}</ds-button>
   </ds-card>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
+import { showShoutsPubliclyMutation } from '~/graphql/User'
 
 export default {
+  data() {
+    return {
+      shoutsAllowed: false,
+    }
+  },
   computed: {
     ...mapGetters({
       currentUser: 'auth/user',
     }),
-    shoutsAllowed: {
-      get() {
-        return this.currentUser.shoutsAllowed || false
-      },
-      set(value) {
-        return value
-      },
+    disabled() {
+      return this.shoutsAllowed === this.currentUser.showShoutsPublicly
     },
   },
+  created() {
+    this.shoutsAllowed = this.currentUser.showShoutsPublicly || false
+  },
   methods: {
-    submit() {
-      console.log('wohoo')
+    ...mapMutations({
+      setCurrentUser: 'auth/SET_USER',
+    }),
+    async submit() {
+      try {
+        await this.$apollo.mutate({
+          mutation: showShoutsPubliclyMutation(),
+          variables: {
+            id: this.currentUser.id,
+            showShoutsPublicly: this.shoutsAllowed,
+          },
+          update: (_, { data: { UpdateUser } }) => {
+            const { showShoutsPublicly } = UpdateUser
+            this.setCurrentUser({
+              ...this.currentUser,
+              showShoutsPublicly,
+            })
+            this.$toast.success(this.$t('settings.privacy.success-update'))
+          },
+        })
+      } catch (error) {
+        this.$toast.error(error.message)
+      }
     },
   },
 }
