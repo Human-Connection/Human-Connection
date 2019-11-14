848e7d76dabd911ba96978715f9a6609619cd1a7
Alina Beck <alina.beck@mail.com>
2019-11-14 14:11:00 +0300

    move icons to main assets folder

--- webapp/assets/_new/icons/index.js
+++ webapp/assets/_new/icons/index.js
@@ -1,10 +1,13 @@
 const svgFileList = require.context('./svgs', true, /\.svg/)
 const icons = {}
+const iconNames = []
 
 svgFileList.keys().forEach(fileName => {
   const svgCode = svgFileList(fileName)
   const iconName = fileName.replace('./', '').replace('.svg', '')
   icons[iconName] = svgCode
+  iconNames.push(iconName)
 })
 
+export { iconNames }
 export default icons
