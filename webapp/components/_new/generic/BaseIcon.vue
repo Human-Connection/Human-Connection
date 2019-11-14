b52480b8d417b154aa8708f0d6bdab6b01e621c6
Alina Beck <alina.beck@mail.com>
2019-11-14 13:04:49 +0300

    add BaseIcon component

---
+++ webapp/components/_new/generic/BaseIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <span v-if="svgIcon">
+    <component :is="svgIcon" />
+  </span>
+</template>
+
+<script>
+import icons from '~/view/assets/icons'
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    svgIcon() {
+      const icon = icons[this.name]
+      if (!icon) {
+        return false
+      }
+      /*
+      a Vue component needs a render function,
+      so we check if there is a render function directly on the icon –
+      otherwise it is wrapped in icon.default
+      */
+      return icon.render ? icon : icon.default
+    },
+  },
+}
+</script>
