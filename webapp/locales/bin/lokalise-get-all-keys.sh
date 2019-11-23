0034abb2744ab74fbcd922579047386297e2da6a
Ewald Arnold <github@ewald-arnold.de>
2019-11-23 09:15:59 +0100

    add lokalise keys and diff

--- webapp/locales/bin/lokalise-get-all-keys.sh
+++ webapp/locales/bin/lokalise-get-all-keys.sh
@@ -1,1 +1,1 @@
-~/bin/lokalise2 key list --token $MY_LOKALISE_TOKEN --project-id 556252725c18dd752dd546.13222042 
+~/bin/lokalise2 key list --token $MY_LOKALISE_TOKEN --project-id 556252725c18dd752dd546.13222042 --filter-filenames 'webapp/locales/%LANG_ISO%.json'  
