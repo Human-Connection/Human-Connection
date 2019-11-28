973d3f899a245186fe8f7aa8be286c12220f73a2
roschaefer <git@roschaefer.de>
2019-11-28 14:02:11 +0100

    Remove dots of gmail addresses
    I had a chat with our moderator Daniel. He asks us to remove dots from
    gmail accounts. He finds it more consistent and he has no problem to
    write a mail to a gmail address without dots. He is OK to save the
    email address different from how a user memorizes it.

--- webapp/components/utils/NormalizeEmail.js
+++ webapp/components/utils/NormalizeEmail.js
@@ -2,7 +2,7 @@ import { normalizeEmail } from 'validator'
 
 export default email =>
   normalizeEmail(email, {
-    gmail_remove_dots: false,
+    // gmail_remove_dots: false, default
     gmail_remove_subaddress: false,
     // gmail_convert_googlemaildotcom: true, default
     outlookdotcom_remove_subaddress: false,
