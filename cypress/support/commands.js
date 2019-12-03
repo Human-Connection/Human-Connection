77fb948c147ddd5f30cc9f0712556c0bfdb3ac80
roschaefer <git@roschaefer.de>
2019-12-03 22:03:35 +0100

    Fix cypress tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -18,7 +18,7 @@ import helpers from "./helpers";
 import users from "../fixtures/users.json";
 import { GraphQLClient, request } from 'graphql-request'
 import { gql } from '../../backend/src/helpers/jest'
-import { GRAPHQL_URI } from '../../backend/src/config'
+import config from '../../backend/src/config'
 
 const switchLang = name => {
   cy.get(".locale-menu").click();
@@ -31,7 +31,7 @@ const authenticatedHeaders = async (variables) => {
       login(email: $email, password: $password)
     }
   `
-  const response = await request(GRAPHQL_URI, mutation, variables)
+  const response = await request(config.GRAPHQL_URI, mutation, variables)
   return { authorization: `Bearer ${response.login}` }
 }
 
@@ -100,8 +100,7 @@ Cypress.Commands.add(
   'authenticateAs',
   async ({email, password}) => {
     const headers = await authenticatedHeaders({ email, password })
-    console.log(headers)
-    return new GraphQLClient(GRAPHQL_URI, { headers })
+    return new GraphQLClient(config.GRAPHQL_URI, { headers })
   }
 )
 
