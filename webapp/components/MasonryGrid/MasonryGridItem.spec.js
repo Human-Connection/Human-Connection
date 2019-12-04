b1955e64f48e4fbdd9f1732810c8d706b7c4865a
Alina Beck <alina.beck@mail.com>
2019-12-04 16:57:42 +0300

    improve tests, following @roschaefer suggestions

--- webapp/components/MasonryGrid/MasonryGridItem.spec.js
+++ webapp/components/MasonryGrid/MasonryGridItem.spec.js
@@ -8,23 +8,41 @@ config.stubs['ds-grid-item'] = '<span><slot /></span>'
 describe('MasonryGridItem', () => {
   let wrapper
 
-  beforeEach(() => {
-    wrapper = mount(MasonryGridItem, { localVue })
+  describe('given an imageAspectRatio', () => {
+    it('sets the initial rowSpan to 13 when the ratio is higher than 1.3', () => {
+      const propsData = { imageAspectRatio: 2 }
+      wrapper = mount(MasonryGridItem, { localVue, propsData })
+
+      expect(wrapper.vm.rowSpan).toBe(13)
+    })
+
+    it('sets the initial rowSpan to 15 when the ratio is between 1.3 and 1', () => {
+      const propsData = { imageAspectRatio: 1.1 }
+      wrapper = mount(MasonryGridItem, { localVue, propsData })
+
+      expect(wrapper.vm.rowSpan).toBe(15)
+    })
+
+    it('sets the initial rowSpan to 18 when the ratio is between 1 and 0.7', () => {
+      const propsData = { imageAspectRatio: 0.7 }
+      wrapper = mount(MasonryGridItem, { localVue, propsData })
+
+      expect(wrapper.vm.rowSpan).toBe(18)
+    })
+
+    it('sets the initial rowSpan to 25 when the ratio is lower than 0.7', () => {
+      const propsData = { imageAspectRatio: 0.3 }
+      wrapper = mount(MasonryGridItem, { localVue, propsData })
+
+      expect(wrapper.vm.rowSpan).toBe(25)
+    })
   })
 
-  it('parent emits "calculating-item-height" when starting calculation', async () => {
-    wrapper.vm.calculateItemHeight()
-    await wrapper.vm.$nextTick()
+  describe('given no aspect ratio', () => {
+    it('sets the initial rowSpan to 8 when not given an imageAspectRatio', () => {
+      wrapper = mount(MasonryGridItem, { localVue })
 
-    const firstEmittedFunction = wrapper.vm.$parent.__emittedByOrder[0]
-    expect(firstEmittedFunction.name).toBe('calculating-item-height')
-  })
-
-  it('parent emits "finished-calculating-item-height" after the calculation', async () => {
-    wrapper.vm.calculateItemHeight()
-    await wrapper.vm.$nextTick()
-
-    const secondEmittedFunction = wrapper.vm.$parent.__emittedByOrder[1]
-    expect(secondEmittedFunction.name).toBe('finished-calculating-item-height')
+      expect(wrapper.vm.rowSpan).toBe(8)
+    })
   })
 })
