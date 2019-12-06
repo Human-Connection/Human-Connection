3bc944b06da64db22e9c493c2044f6dfa7dc5f17
mattwr18 <mattwr18@gmail.com>
2019-12-06 18:56:20 +0100

    Finish refactoring posts resolver

--- backend/src/schema/resolvers/posts.js
+++ backend/src/schema/resolvers/posts.js
@@ -124,8 +124,8 @@ export default {
         return createPostTransactionResponse.records.map(record => record.get('post').properties)
       })
       try {
-        const [posts] = await writeTxResultPromise
-        return posts
+        const [post] = await writeTxResultPromise
+        return post
       } catch (e) {
         if (e.code === 'Neo.ClientError.Schema.ConstraintValidationFailed')
           throw new UserInputError('Post with this slug already exists!')
@@ -184,23 +184,25 @@ export default {
 
     DeletePost: async (object, args, context, resolveInfo) => {
       const session = context.driver.session()
-      try {
-        // we cannot set slug to 'UNAVAILABE' because of unique constraints
-        const transactionRes = await session.run(
+      const writeTxResultPromise = session.writeTransaction(async transaction => {
+        const deletePostTransactionResponse = await transaction.run(
           `
-        MATCH (post:Post {id: $postId})
-        OPTIONAL MATCH (post)<-[:COMMENTS]-(comment:Comment)
-        SET post.deleted        = TRUE
-        SET post.content        = 'UNAVAILABLE'
-        SET post.contentExcerpt = 'UNAVAILABLE'
-        SET post.title          = 'UNAVAILABLE'
-        SET comment.deleted     = TRUE
-        REMOVE post.image
-        RETURN post
-      `,
+            MATCH (post:Post {id: $postId})
+            OPTIONAL MATCH (post)<-[:COMMENTS]-(comment:Comment)
+            SET post.deleted        = TRUE
+            SET post.content        = 'UNAVAILABLE'
+            SET post.contentExcerpt = 'UNAVAILABLE'
+            SET post.title          = 'UNAVAILABLE'
+            SET comment.deleted     = TRUE
+            REMOVE post.image
+            RETURN post
+          `,
           { postId: args.id },
         )
-        const [post] = transactionRes.records.map(record => record.get('post').properties)
+        return deletePostTransactionResponse.records.map(record => record.get('post').properties)
+      })
+      try {
+        const [post] = await writeTxResultPromise
         return post
       } finally {
         session.close()
@@ -210,21 +212,24 @@ export default {
       const { to, data } = params
       const { user } = context
       const session = context.driver.session()
-      try {
-        const transactionRes = await session.run(
-          `MATCH (userFrom:User {id: $user.id}), (postTo:Post {id: $to.id})
-        MERGE (userFrom)-[emotedRelation:EMOTED {emotion: $data.emotion}]->(postTo)
-        RETURN userFrom, postTo, emotedRelation`,
+      const writeTxResultPromise = session.writeTransaction(async transaction => {
+        const addPostEmotionsTransactionResponse = await transaction.run(
+          `
+          MATCH (userFrom:User {id: $user.id}), (postTo:Post {id: $to.id})
+          MERGE (userFrom)-[emotedRelation:EMOTED {emotion: $data.emotion}]->(postTo)
+          RETURN userFrom, postTo, emotedRelation`,
           { user, to, data },
         )
-
-        const [emoted] = transactionRes.records.map(record => {
+        return addPostEmotionsTransactionResponse.records.map(record => {
           return {
             from: { ...record.get('userFrom').properties },
             to: { ...record.get('postTo').properties },
             ...record.get('emotedRelation').properties,
           }
         })
+      })
+      try {
+        const [emoted] = await writeTxResultPromise
         return emoted
       } finally {
         session.close()
@@ -234,20 +239,25 @@ export default {
       const { to, data } = params
       const { id: from } = context.user
       const session = context.driver.session()
-      try {
-        const transactionRes = await session.run(
-          `MATCH (userFrom:User {id: $from})-[emotedRelation:EMOTED {emotion: $data.emotion}]->(postTo:Post {id: $to.id})
-        DELETE emotedRelation
-        RETURN userFrom, postTo`,
+      const writeTxResultPromise = session.writeTransaction(async transaction => {
+        const removePostEmotionsTransactionResponse = await transaction.run(
+          `
+            MATCH (userFrom:User {id: $from})-[emotedRelation:EMOTED {emotion: $data.emotion}]->(postTo:Post {id: $to.id})
+            DELETE emotedRelation
+            RETURN userFrom, postTo
+          `,
           { from, to, data },
         )
-        const [emoted] = transactionRes.records.map(record => {
+        return removePostEmotionsTransactionResponse.records.map(record => {
           return {
             from: { ...record.get('userFrom').properties },
             to: { ...record.get('postTo').properties },
             emotion: data.emotion,
           }
         })
+      })
+      try {
+        const [emoted] = await writeTxResultPromise
         return emoted
       } finally {
         session.close()
@@ -351,21 +361,28 @@ export default {
     relatedContributions: async (parent, params, context, resolveInfo) => {
       if (typeof parent.relatedContributions !== 'undefined') return parent.relatedContributions
       const { id } = parent
-      const statement = `
-      MATCH (p:Post {id: $id})-[:TAGGED|CATEGORIZED]->(categoryOrTag)<-[:TAGGED|CATEGORIZED]-(post:Post)
-      WHERE NOT post.deleted AND NOT post.disabled
-      RETURN DISTINCT post
-      LIMIT 10
-      `
-      let relatedContributions
       const session = context.driver.session()
+
+      const writeTxResultPromise = session.writeTransaction(async transaction => {
+        const relatedContributionsTransactionResponse = await transaction.run(
+          `
+            MATCH (p:Post {id: $id})-[:TAGGED|CATEGORIZED]->(categoryOrTag)<-[:TAGGED|CATEGORIZED]-(post:Post)
+            WHERE NOT post.deleted AND NOT post.disabled
+            RETURN DISTINCT post
+            LIMIT 10
+          `,
+          { id },
+        )
+        return relatedContributionsTransactionResponse.records.map(
+          record => record.get('post').properties,
+        )
+      })
       try {
-        const result = await session.run(statement, { id })
-        relatedContributions = result.records.map(r => r.get('post').properties)
+        const relatedContributions = await writeTxResultPromise
+        return relatedContributions
       } finally {
         session.close()
       }
-      return relatedContributions
     },
   },
 }
