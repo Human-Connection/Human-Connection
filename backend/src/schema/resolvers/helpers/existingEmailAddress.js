d39e702e70671ea8271d136227e12fc07b535d7a
mattwr18 <mattwr18@gmail.com>
2019-12-11 18:19:40 +0100

    Update exisitingEmailAddress

--- backend/src/schema/resolvers/helpers/existingEmailAddress.js
+++ backend/src/schema/resolvers/helpers/existingEmailAddress.js
@@ -1,25 +1,29 @@
 import { UserInputError } from 'apollo-server'
 
 export default async function alreadyExistingMail({ args, context }) {
-  const cypher = `
-    MATCH (email:EmailAddress {email: $email})
-    OPTIONAL MATCH (email)-[:BELONGS_TO]-(user)
-    RETURN email, user
-  `
-  let transactionRes
   const session = context.driver.session()
   try {
-    transactionRes = await session.run(cypher, { email: args.email })
+    const existingEmailAddressTxPromise = session.writeTransaction(async transaction => {
+      const existingEmailAddressTransactionResponse = await transaction.run(
+        `
+          MATCH (email:EmailAddress {email: $email})
+          OPTIONAL MATCH (email)-[:BELONGS_TO]-(user)
+          RETURN email, user
+        `,
+        { email: args.email },
+      )
+      return existingEmailAddressTransactionResponse.records.map(record => {
+        return {
+          alreadyExistingEmail: record.get('email').properties,
+          user: record.get('user') && record.get('user').properties,
+        }
+      })
+    })
+    const [emailBelongsToUser] = await existingEmailAddressTxPromise
+    const { alreadyExistingEmail, user } = emailBelongsToUser || {}
+    if (user) throw new UserInputError('A user account with this email already exists.')
+    return alreadyExistingEmail
   } finally {
     session.close()
   }
-  const [result] = transactionRes.records.map(record => {
-    return {
-      alreadyExistingEmail: record.get('email').properties,
-      user: record.get('user') && record.get('user').properties,
-    }
-  })
-  const { alreadyExistingEmail, user } = result || {}
-  if (user) throw new UserInputError('A user account with this email already exists.')
-  return alreadyExistingEmail
 }
