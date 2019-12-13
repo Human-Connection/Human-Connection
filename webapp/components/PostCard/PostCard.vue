792ae8d598e39049478e1833f1400f33cb067289
Alexander Friedland <ogerly@users.noreply.github.com>
2019-12-13 13:39:51 +0100

    change  line 152
    ".ds-card-imag" TO ".ds-card-image"

--- webapp/components/PostCard/PostCard.vue
+++ webapp/components/PostCard/PostCard.vue
@@ -149,7 +149,7 @@ export default {
   mounted() {
     const width = this.$el.offsetWidth
     const height = Math.min(width / this.post.imageAspectRatio, 2000)
-    const imageElement = this.$el.querySelector('.ds-card-imag')
+    const imageElement = this.$el.querySelector('.ds-card-image')
 
     if (imageElement) {
       imageElement.style.height = `${height}px`
