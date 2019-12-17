92856afa3caeb1dcf8a538bc9d21c5d4428d2a60
Alina Beck <alina.beck@mail.com>
2019-12-17 12:51:05 +0530

    sort storybook stories alphabetically

--- webapp/storybook/config.js
+++ webapp/storybook/config.js
@@ -55,4 +55,12 @@ function loadStories() {
   req.keys().forEach(req)
 }
 
+// sort stories alphabetically
+addParameters({
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, { numeric: true }),
+  }
+})
+
 configure(loadStories, module)
