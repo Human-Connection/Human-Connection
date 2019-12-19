72f2aff455b18c69aa0c1306a0c5f0ac1df9e815
Alina Beck <alina.beck@mail.com>
2019-12-19 11:43:14 +0530

    add color variants to CounterIcon

--- webapp/components/_new/generic/CounterIcon/CounterIcon.vue
+++ webapp/components/_new/generic/CounterIcon/CounterIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <span class="counter-icon">
     <base-icon :name="icon" />
-    <span class="count">{{ cappedCount }}</span>
+    <span :class="counterClass">{{ cappedCount }}</span>
   </span>
 </template>
 
@@ -10,12 +10,20 @@ export default {
   props: {
     icon: { type: String, required: true },
     count: { type: Number, required: true },
+    danger: { type: Boolean, default: false },
   },
   computed: {
     cappedCount() {
       return this.count <= 99 ? this.count : '99+'
-    }
-  }
+    },
+    counterClass() {
+      let counterClass = 'count'
+      if (this.danger) counterClass += ' --danger'
+      if (this.count === 0) counterClass += ' --inactive'
+
+      return counterClass
+    },
+  },
 }
 </script>
 
@@ -42,6 +50,14 @@ export default {
     font-size: 10px;
     line-height: 1;
     text-align: center;
+
+    &.--danger {
+      background-color: $color-danger;
+    }
+
+    &.--inactive {
+      background-color: $color-neutral-60;
+    }
   }
 }
 </style>
