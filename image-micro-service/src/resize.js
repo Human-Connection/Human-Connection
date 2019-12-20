c3b80d920a19eed3303333947aebed3458fbfbfc
mattwr18 <mattwr18@gmail.com>
2019-12-20 15:28:35 +0100

    Initial commit
    - Maybe this isn't helpful at all, but since no one else has opened up a
    WIP PR, I though I would try to learn a bit before the holidays. I hope
    we can use this as a starting point to start a discussion about this.
    
    - Following tutorial https://malcoded.com/posts/nodejs-image-resize-express-sharp/

---
+++ image-micro-service/src/resize.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const sharp = require('sharp')
+
+module.exports = function resize(path, format, width, height) {
+  const readStream = fs.createReadStream(path)
+  let transform = sharp()
+
+  if (format) transform = transform.toFormat(format)
+  if (width || height) transform = transform.resize(width, height)
+
+  return readStream.pipe(transform)
+}
---
+++ image-micro-service/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const resize = require('./resize')
+const server = express()
+
+server.get('/', (req, res) => {
+  const widthString = req.query.width
+  const heightString = req.query.height
+  const format = req.query.format
+  let width, height
+  if(widthString)  width = parseInt(widthString)
+  if (heightString) height = parseInt(heightString)
+  
+  res.type(`image/${format || 'png'}`)
+  resize('./static/img/sign-up/onourjourney.png', format, width, height).pipe(res)
+})
+
+server.listen(4001, () => {
+  console.log('This is a kick ass image micro service')
+})
