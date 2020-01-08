5d2af860dbcb999a922e58f29f2f9ac78f609722
Alina Beck <alina.beck@mail.com>
2020-01-08 13:53:02 +0530

    wrap create-post link around button on user profile

--- webapp/pages/profile/_id/_slug.vue
+++ webapp/pages/profile/_id/_slug.vue
@@ -215,19 +215,21 @@
 
           <ds-grid-item :row-span="2" column-span="fullWidth">
             <ds-space centered>
-              <base-button
-                v-if="myProfile"
-                v-tooltip="{
-                  content: $t('contribution.newPost'),
-                  placement: 'left',
-                  delay: { show: 500 },
-                }"
-                :path="{ name: 'post-create' }"
-                class="profile-post-add-button"
-                icon="plus"
-                circle
-                primary
-              />
+              <nuxt-link :to="{ name: 'post-create' }">
+                <base-button
+                  v-if="myProfile"
+                  v-tooltip="{
+                    content: $t('contribution.newPost'),
+                    placement: 'left',
+                    delay: { show: 500 },
+                  }"
+                  :path="{ name: 'post-create' }"
+                  class="profile-post-add-button"
+                  icon="plus"
+                  circle
+                  primary
+                />
+              </nuxt-link>
             </ds-space>
           </ds-grid-item>
 
