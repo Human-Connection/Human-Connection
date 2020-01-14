76b20fa8bdf0c3b0af7375c4e9c6820fcf70a268
mattwr18 <mattwr18@gmail.com>
2020-01-14 13:15:49 +0100

    Fix: Cypress Neo4j authentication without Docker
    - It's a bit of a pain to need to run Docker just to run the cypress
    tests, and while it would be nice to use the same .env, we have an issue
    with relative paths, dotenv, and cypress playing nice together.

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 import path from 'path'
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+dotenv.config({ path: path.resolve(__dirname, '../../.env'), debug: true })
 
 const {
   MAPBOX_TOKEN,
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -13,8 +13,5 @@
 
 const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = on => {
-  // (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
   on('file:preprocessor', cucumber())
 }
