abee6ed766c4dd2cc2adb5fbbdd63d34ec6c4bdb
mattwr18 <mattwr18@gmail.com>
2020-01-14 20:52:19 +0100

    Remove debug for dotenv, cp cypress env for travis

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import path from 'path'
-dotenv.config({ path: path.resolve(__dirname, '../../.env'), debug: true })
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 const {
   MAPBOX_TOKEN,
