16ad565ec8c3ff83d6b363861ada7ea0a29d3ce9
Moriz Wahl <moriz.wahl@gmx.de>
2020-02-11 09:27:28 +0100

    partially working, index page does not update when route is changed

---
+++ webapp/components/generic/SearchTag/SearchTag.vue
@@ -0,0 +1,23 @@
+<template>
+  <ds-flex class="search-tag">
+    <ds-flex-item class="search-tag-label">
+      <ds-text>#{{ option.id }}</ds-text>
+    </ds-flex-item>
+  </ds-flex>
+</template>
+<script>
+ export default {
+     name: 'SearchTag',
+     props: {
+	 option: { type: Object, required: true },
+     },
+ }
+</script>
+<style lang="scss">
+ .search-tag {
+     width: 100%;
+ }
+ .search-tag-label {
+     color: $text-color-primary;
+ }
+</style>
