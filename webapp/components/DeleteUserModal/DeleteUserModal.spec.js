ee06039982666b869a82e915a926263d4fc7b202
ogerly <landmatrosen@googlemail.com>
2020-03-05 09:12:46 +0100

    test: Added Spec for DeleteUserModal

---
+++ webapp/components/DeleteUserModal/DeleteUserModal.spec.js
@@ -0,0 +1,13 @@
+import { config, shallowMount, mount } from '@vue/test-utils'
+
+import ConfirmModal from './DeleteUserModal.vue'
+
+const localVue = global.localVue
+
+describe('DisableModal.vue', () => {
+    let mocks
+    let propsData
+    let wrapper
+
+    
+})
