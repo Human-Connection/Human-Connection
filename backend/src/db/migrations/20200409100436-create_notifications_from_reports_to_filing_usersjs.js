20dd610e27cf6f59027f218f52bb518498ac5694
Wolfgang Huß <wolle.huss@pjannto.com>
2020-04-09 10:38:50 +0200

    Define database migration to create implemented notifications
    - Create notifications for all old filed reports to all of their submitters.

---
+++ backend/src/db/migrations/20200409100436-create_notifications_from_reports_to_filing_usersjs.js
@@ -0,0 +1,45 @@
+import { getDriver } from '../../db/neo4j'
+
+export const description = 'Create notifications for all old filed reports to all of their submitters.'
+
+export async function up(next) {
+  const driver = getDriver()
+  const session = driver.session()
+  const transaction = session.beginTransaction()
+  const updateDeletedUserAttributes = await transaction.run(`
+    MATCH (submitter:User)-[filed:FILED]->(report:Report)
+    WHERE NOT (submitter)<-[:NOTIFIED]-(report)
+    CREATE (submitter)<-[notification:NOTIFIED]-(report)
+    SET notification.createdAt = filed.createdAt,
+      notification.updatedAt = notification.createdAt,
+      notification.read = FALSE,
+      notification.reason = 'filed_report_on_resource'
+    RETURN notification {.*};
+  `)
+
+  try {
+    // Implement your migration here.
+    const notifications = await updateDeletedUserAttributes.records.map((record) =>
+      record.get('notification'),
+    )
+    // eslint-disable-next-line no-console
+    console.log(notifications)
+    await transaction.commit()
+    next()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+    await transaction.rollback()
+    // eslint-disable-next-line no-console
+    console.log('rolled back')
+    throw new Error(error)
+  } finally {
+    session.close()
+  }
+}
+
+export async function down(next) {
+  // eslint-disable-next-line no-console
+  console.log('Irreversible migration')
+  next()
+}
