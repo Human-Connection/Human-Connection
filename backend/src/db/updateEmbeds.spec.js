a8a5ed40ab9cb2dcd5a19e3092c7d6ac38e3892c
Raphael Beer <raphael.beer@protonmail.com>
2020-04-15 13:53:30 +0200

    Review: same data structure for both sample sets

--- backend/src/db/updateEmbeds.spec.js
+++ backend/src/db/updateEmbeds.spec.js
@@ -1,17 +1,22 @@
 import { transform } from './updateEmbeds'
-import legacySamples from './test/20200402150445-update-embeds/samples_legacy.json'
-import targetSamples from './test/20200402150445-update-embeds/samples_target.json'
+import storybookSamples from './test/20200402150445-update-embeds/samples_storybook.json'
 import productionSamples from './test/20200402150445-update-embeds/samples_production.json'
 
 describe('updateEmbeds', () => {
   describe('transform', () => {
-    Object.keys(legacySamples).forEach((key) => {
-      it(`converts the ${key} sample`, () => {
-        expect(transform(legacySamples[key])).toBe(targetSamples[key])
+    describe('synthetic storybook samples', () => {
+      storybookSamples.forEach((sample) => {
+        it(`transforms ${sample.type} content`, () => {
+          expect(transform(sample.legacy)).toBe(sample.target)
+        })
       })
     })
-    it('converts all production samples', () => {
-      productionSamples.forEach((sample) => expect(transform(sample.legacy)).toBe(sample.target))
+    describe('production samples', () => {
+      productionSamples.forEach((sample) => {
+        it(`transforms ${sample.type} content`, () => {
+          expect(transform(sample.legacy)).toBe(sample.target)
+        })
+      })
     })
   })
 })
