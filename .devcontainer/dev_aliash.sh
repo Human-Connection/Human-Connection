155ada71df42c1c7c4e8592a64db5feda608a655
Edward Raigosa <wenlock@github.com>
2020-07-04 17:05:42 +0000

    tune mount points

---
+++ .devcontainer/dev_aliash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
+export GIT_ROOT="$(cd "${SCRIPT_DIR}" && git rev-parse --show-toplevel)"
+#
+# source this file for some environment alias
+alias dc='docker-compose --file ${GIT_ROOT}/docker-compose.yml --file ${GIT_ROOT}/.devcontainer/docker-compose.yml'
--- deployment/codespaces/bootstrap.sh
+++ deployment/codespaces/bootstrap.sh
@@ -11,22 +11,22 @@ export GIT_ROOT="$(cd "${SCRIPT_DIR}" && git rev-parse --show-toplevel)"
 
 #
 # startup neo4j db
-cd "${GIT_ROOT}"
-docker-compose --file ./docker-compose.yml up -d neo4j
+# cd "${GIT_ROOT}"
+# docker-compose --file ./docker-compose.yml up -d neo4j
 
 #
 # setup the backend
-cd "./backend"
-cp .env.template .env
-yarn install
-cd "${GIT_ROOT}"
+# cd "./backend"
+# cp .env.template .env
+# yarn install
+# cd "${GIT_ROOT}"
 
 #
 # setup the frontend
-cd "./webapp"
-cp .env.template .env
-yarn install
-cd "${GIT_ROOT}"
+# cd "./webapp"
+# cp .env.template .env
+# yarn install
+# cd "${GIT_ROOT}"
 
 # (cd "${GIT_ROOT}/backend" && /sbin/tini -g -- yarn run dev &)
 # (cd "${GIT_ROOT}/frontend" && /sbin/tini -g -- yarn run dev &)
