aca06f7679e30310db759be043137a8636cf9c0f
Edward Raigosa <wenlock@github.com>
2020-07-05 16:05:49 +0000

    adding an alias and switching commands

--- .devcontainer/profile_devcontainer_alias.sh
+++ .devcontainer/profile_devcontainer_alias.sh
@@ -4,4 +4,4 @@ export SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
 export GIT_ROOT="$(cd "${SCRIPT_DIR}" && git rev-parse --show-toplevel)"
 #
 # source this file for some environment alias
-alias dc='docker-compose --file ${GIT_ROOT}/docker-compose.yml --file ${GIT_ROOT}/.devcontainer/docker-compose.yml'
+alias dc='docker-compose --file ${GIT_ROOT}/docker-compose.yml --file ${GIT_ROOT}/.devcontainer/docker-compose.yml --project-name vsonline-compose'
