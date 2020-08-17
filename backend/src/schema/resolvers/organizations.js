0ec2e87e391482cbcb421d5af70a09d51299cfa7
Moriz Wahl <moriz.wahl@gmx.de>
2020-08-17 19:41:12 +0200

    Location is not set for organizations, Categories are deleted after editing. Must be fixed in next commit

--- backend/src/schema/resolvers/organizations.js
+++ backend/src/schema/resolvers/organizations.js
@@ -2,25 +2,21 @@ import { v4 as uuid } from 'uuid'
 import { neo4jgraphql } from 'neo4j-graphql-js'
 import { mergeImage } from './images/images'
 import Resolver from './helpers/Resolver'
+import createOrUpdateLocations from './users/location'
 
 
 export default {
   Query: {
     Organization: async (object, params, context, resolveInfo) => {
-      console.log("Organization", params)
-      let tmp = await neo4jgraphql(object, params, context, resolveInfo)
-      console.log(tmp)
-      return tmp
+      return neo4jgraphql(object, params, context, resolveInfo)
     },
   },
   Mutation: {
     CreateOrganization: async (object, params, context, resolveInfo) => {
-      console.log("Incoming params:", params)
       params.id = params.id || uuid()
       const { categoryIds, image: imageInput } = params
       delete params.categoryIds
       delete params.image
-      console.log("Outgoing params:", params)
       const session = context.driver.session()
       const writeTxResultPromise = session.writeTransaction(async (transaction) => {
         const createOrganizationTransactionResponse = await transaction.run(
@@ -48,11 +44,65 @@ export default {
       })
       try {
         const organization = await writeTxResultPromise
+        await createOrUpdateLocations(params.id, params.locationName, session)
         return organization
       } catch (e) {
         if (e.code === 'Neo.ClientError.Schema.ConstraintValidationFailed')
           throw new UserInputError('Organization with this slug already exists!')
         throw new Error(e)
+      } finally {
+        session.close()
+      }
+    },
+    UpdateOrganization: async (_parent, params, context, _resolveInfo) => {
+      const { categoryIds, image: imageInput } = params
+      console.log(categoryIds)
+      delete params.categoryIds
+      delete params.image
+      const session = context.driver.session()
+      let updateOrganizationCypher = `
+        MATCH (organization:Organization { id: $params.id })
+        SET organization += $params
+        SET organization.updatedAt = toString(datetime())
+        WITH organization
+      `
+
+      if (categoryIds && categoryIds.length) {
+        const cypherDeletePreviousRelations = `
+          MATCH (organization:Organization { id: $params.id })-[previousRelations:CATEGORIZED]->(category:Category)
+          DELETE previousRelations
+          RETURN organization, category
+          `
+
+        await session.writeTransaction((transaction) => {
+          return transaction.run(cypherDeletePreviousRelations, { params })
+        })
+
+        updateOrganizationCypher += `
+          UNWIND $categoryIds AS categoryId
+          MATCH (category:Category {id: categoryId})
+          MERGE (orgnaization)-[:CATEGORIZED]->(category)
+          WITH organization
+        `
+      }
+
+      updateOrganizationCypher += `RETURN organization {.*}`
+      console.log(updateOrganizationCypher)
+      const updateOrganizationVariables = { categoryIds, params }
+      console.log(updateOrganizationVariables)
+      try {
+        const writeTxResultPromise = session.writeTransaction(async (transaction) => {
+          const updateOrganizationTransactionResponse = await transaction.run(
+            updateOrganizationCypher,
+            updateOrganizationVariables,
+          )
+          const [organization] = updateOrganizationTransactionResponse.records.map((record) => record.get('organization'))
+          await mergeImage(organization, 'HERO_IMAGE', imageInput, { transaction })
+          return organization
+        })
+        const organization = await writeTxResultPromise
+        await createOrUpdateLocations(params.id, params.locationName, session)
+        return organization
       } finally {
         session.close()
       }
@@ -68,6 +118,7 @@ export default {
       hasOne: {
         creator: '<-[:CREATED]-(related:User)',
         image: '-[:HERO_IMAGE]->(related:Image)',
+        location: '-[:IS_IN]->(related:Location)',
       },
       count: {
       },
---
+++ webapp/pages/organization/edit/_id.vue
@@ -0,0 +1,46 @@
+<template>
+  <ds-flex :width="{ base: '100%' }" gutter="base">
+    <ds-flex-item :width="{ base: '100%', md: 3 }">
+      <organization-form :organization="organization" />
+    </ds-flex-item>
+    <ds-flex-item :width="{ base: '100%', md: 1 }">&nbsp;</ds-flex-item>
+  </ds-flex>
+</template>
+
+<script>
+ import OrganizationForm from '~/components/OrganizationForm/OrganizationForm'
+ import OrganizationQuery from '~/graphql/OrganizationQuery'
+ import { mapGetters } from 'vuex'
+
+ export default {
+   components: {
+     OrganizationForm,
+   },
+   computed: {
+     ...mapGetters({
+       user: 'auth/user',
+     }),
+   },
+   async asyncData(context) {
+     const {
+       app,
+       store,
+       error,
+       params: { id },
+     } = context
+     const client = app.apolloProvider.defaultClient
+     const {
+       data: {
+         Organization: [organization],
+       },
+     } = await client.query({
+       query: OrganizationQuery(app.$i18n),
+       variables: { id },
+     })
+     if (organization.creator.id !== store.getters['auth/user'].id) {
+       error({ statusCode: 403, message: 'error-pages.cannot-edit-organization' })
+     }
+     return { organization }
+   },
+ }
+</script>
