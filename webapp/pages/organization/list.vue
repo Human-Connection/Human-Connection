83d4deec81fe78e5445ecfee5ff5f9cdd47e2c7f
Moriz Wahl <moriz.wahl@gmx.de>
2020-09-03 16:36:37 +0200

    simple organization list

---
+++ webapp/pages/organization/list.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="organization-list">
+    <base-card v-if="Organization && Organization.length">
+      <div align="right">
+        <nuxt-link to="/organization/create">
+          <base-button filled>{{ $t('organizations.add') }}</base-button>
+        </nuxt-link>
+      </div>
+      <h2 class="title">{{ $t('organizations.name') }}</h2>
+      <ul v-if="Organization">
+        <li v-for="orga in Organization" :key="orga.id" class="item">
+          <nuxt-link :to="organizationLink(orga)">
+            <base-card :wideContent="true">
+              <ds-text>{{ orga.name  }}</ds-text>
+              <ds-text v-if="orga.location" align="left" color="soft" size="small">
+                <base-icon name="map-marker" />
+                {{ orga.location.name }}            
+              </ds-text>
+              <div class="categories">
+                <hc-category
+                  v-for="category in orga.categories"
+                  :key="category.id"
+                  :icon="category.icon"
+                  :name="$t(`contribution.category.name.${category.slug}`)"
+                />
+              </div>
+            </base-card>
+          </nuxt-link>
+        </li>
+      </ul>
+    </base-card>
+  </div>
+</template>
+<script>
+ import gql from 'graphql-tag'
+ import HcCategory from '~/components/Category'
+
+ export default {
+   components: {
+     HcCategory,
+   },
+   data() {
+     const pageSize = 15
+     return {
+       offset: 0,
+       pageSize,
+       first: pageSize,
+       Organization: [],
+       hasNext: false,
+       filter: null,
+       form: {
+         formData: {
+           query: '',
+         },
+       },
+     }
+   },
+   methods: {
+     organizationLink(orga) {
+       const { id, slug } = orga
+       if (!(id && slug)) return ''
+       return `/organization/${id}/${slug}`
+     },
+   },
+   apollo: {
+     Organization: {
+       query() {
+         return gql`
+           query($first: Int, $offset: Int) {
+             Organization(
+               first: $first
+               offset: $offset
+             ) {
+               id
+               name
+               slug
+               location {
+                 name
+               }
+               categories {
+                 name
+                 icon
+                 slug
+               }
+             }
+           }
+         `
+       },
+       variables() {
+         const { offset, first } = this
+         const variables = { first, offset }
+         return variables
+       },
+       update({ Organization }) {
+         if (!Organization) return []
+         this.hasNext = Organization.length >= this.pageSize
+         if (Organization.length <= 0 && this.offset > 0) return this.Organization // edge case, avoid a blank page
+         return Organization.map((o, i) => Object.assign({}, o, { index: this.offset + i }))
+       },
+     },
+   },
+ }
+</script>
+<style lang="scss">
+</style>
