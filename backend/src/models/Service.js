b1d30262927a3af1b1df879db65becab3d67bd28
Heiker <heiker.araujo@cleteci.com>
2020-10-06 08:00:38 -0400

    adding description to services

--- backend/src/models/Service.js
+++ backend/src/models/Service.js
@@ -3,6 +3,7 @@ import { v4 as uuid } from 'uuid'
 export default {
   id: { type: 'string', primary: true, default: uuid },
   name: { type: 'string', required: true, default: false },
+  description: { type: 'string', required: false, default: 'Not Assigned' },
   createdAt: { type: 'string', isoDate: true, default: () => new Date().toISOString() },
   updatedAt: {
     type: 'string',
